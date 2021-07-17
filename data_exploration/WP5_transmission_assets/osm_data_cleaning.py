7152250b3e56b9a3e42272bff32913f13f25e4f9
davide-f <67809479+davide-f@users.noreply.github.com>
2021-07-17 16:27:23 +0200

    Preliminary cleaning of substations

--- data_exploration/WP5_transmission_assets/osm_data_cleaning.py
+++ data_exploration/WP5_transmission_assets/osm_data_cleaning.py
@@ -9,8 +9,80 @@ import sys
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append("../../scripts")
 
+def prepare_substation_df(df_all_substations):
+    """
+    Prepare raw substations dataframe to the structure compatible with PyPSA-Eur
 
-def clean_data():
+    Parameters
+    ----------
+    df_all_substations : dataframe
+        Raw substations dataframe as downloaded from OpenStreetMap
+    """
+
+    # Modify the naming of the DataFrame columns to adapt to the PyPSA-Eur-like format
+    df_all_substations = df_all_substations.rename(
+        columns = {
+            "id": "bus_id",
+            "tags.voltage": "voltage",
+            # "dc", will be added below
+            "tags.power": "symbol",
+            # "under_construction", will be added below     
+            "tags.substation": "tag_substation",
+            "Country": "country",  # new/different to PyPSA-Eur
+            "Area": "tag_area",
+            "lonlat": "geometry",
+        }
+    )
+
+    # Add longitute (lon) and latitude (lat) coordinates in the dataset
+    df_all_substations["lon"] = df_all_substations["geometry"].x
+    df_all_substations["lat"] = df_all_substations["geometry"].y
+
+    # Add NaN as default
+    df_all_substations["station_id"] = np.nan
+    df_all_substations["dc"] = np.nan
+    df_all_substations["under_construction"] = np.nan
+
+    #Rearrange columns
+    clist = ["bus_id","station_id","voltage","dc","symbol","under_construction","tag_substation",
+            "tag_area","lon", "lat", "geometry","country"]
+    df_all_substations = df_all_substations[clist]
+
+    # add the under_construction and dc
+    df_all_substations["under_construction"] = True
+    df_all_substations["dc"] = False
+
+
+def filter_substations(df_all_substations, tag_substation = "transmission", threshold_voltage = 110000):
+    """
+    Filter substations according to substation type and voltage.
+    Substations corresponding to non-numeric voltages or nan are excluded
+    
+    """
+
+    # filter only entries corresponding to transmission elements ("transmission")
+    df_all_substations = df_all_substations[df_all_substations["tag_substation"] == tag_substation]
+
+    # Covnert voltage to float, or set to nan the value
+    df_all_substations['voltage'] = df_all_substations['voltage'].apply(lambda x: pd.to_numeric(x, errors='coerce')).astype(float)
+
+    # Drop any row with N/A voltage
+    df_all_substations = df_all_substations.dropna(subset=['voltage']) 
+
+    # keep only lines with a voltage no lower than than threshold_voltage
+    df_all_substations = df_all_substations[df_all_substations.voltage >= threshold_voltage]
+
+
+def finalize_substation_types(df_all_substations):
+    """
+    Specify bus_id and voltage columns as integer
+    """
+    # make float to integer
+    df_all_substations["bus_id"] = df_all_substations["bus_id"].astype(int)
+    df_all_substations.loc[:,"voltage"]  = df_all_substations['voltage'].astype(int)
+    
+
+def clean_data(tag_substation = "transmission", threshold_voltage = 110000):
 
     outputfile_partial = os.path.join(os.getcwd(), "data", "africa_all") # Output file directory
 
@@ -22,6 +94,16 @@ def clean_data():
 
     # TODO : Cleaning goes here
 
+    # prepare dataset for substations
+    prepare_substation_df(df_all_substations)
+
+    # filter substations
+    filter_substations(df_all_substations, tag_substation, threshold_voltage)
+
+    # finalize dataframe types
+    finalize_substation_types(df_all_substations)
+
+    # save to csv file
     df_all_substations.to_file(outputfile_partial + "_substations"+ ".geojson", driver="GeoJSON")
 
 
