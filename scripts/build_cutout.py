269a2b283b44cc4df36baba55f818bbd3375b592
Restyled.io <commits@restyled.io>
2021-08-09 19:12:02 +0000

    Restyled by black

--- scripts/build_cutout.py
+++ scripts/build_cutout.py
@@ -106,30 +106,31 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 if __name__ == "__main__":
-    if 'snakemake' not in globals():
+    if "snakemake" not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake('build_cutout', cutout='africa-2013-era5')
+
+        snakemake = mock_snakemake("build_cutout", cutout="africa-2013-era5")
     configure_logging(snakemake)
 
-    cutout_params = snakemake.config['atlite']['cutouts'][snakemake.wildcards.cutout]
+    cutout_params = snakemake.config["atlite"]["cutouts"][snakemake.wildcards.cutout]
 
-    snapshots = pd.date_range(freq='h', **snakemake.config['snapshots'])
+    snapshots = pd.date_range(freq="h", **snakemake.config["snapshots"])
     time = [snapshots[0], snapshots[-1]]
-    cutout_params['time'] = slice(*cutout_params.get('time', time))
+    cutout_params["time"] = slice(*cutout_params.get("time", time))
 
     # If one of the parameters is there
-    if {'x', 'y', 'bounds'}.isdisjoint(cutout_params):
+    if {"x", "y", "bounds"}.isdisjoint(cutout_params):
         # Determine the bounds from bus regions with a buffer of two grid cells
         onshore = gpd.read_file(snakemake.input.regions_onshore)
         offshore = gpd.read_file(snakemake.input.regions_offshore)
         regions = onshore.append(offshore)
-        d = max(cutout_params.get('dx', 0.25), cutout_params.get('dy', 0.25))*2
-        cutout_params['bounds'] = regions.total_bounds + [d, d, d, d]
-    elif {'x', 'y'}.issubset(cutout_params):
-        cutout_params['x'] = slice(*cutout_params['x'])
-        cutout_params['y'] = slice(*cutout_params['y'])
+        d = max(cutout_params.get("dx", 0.25), cutout_params.get("dy", 0.25)) * 2
+        cutout_params["bounds"] = regions.total_bounds + [d, d, d, d]
+    elif {"x", "y"}.issubset(cutout_params):
+        cutout_params["x"] = slice(*cutout_params["x"])
+        cutout_params["y"] = slice(*cutout_params["y"])
 
     logging.info(f"Preparing cutout with parameters {cutout_params}.")
-    features = cutout_params.pop('features', None)
+    features = cutout_params.pop("features", None)
     cutout = atlite.Cutout(snakemake.output[0], **cutout_params)
     cutout.prepare(features=features)
