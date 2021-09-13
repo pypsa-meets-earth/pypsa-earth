219faf50a3b296e688098ba185875f2d622efd34
davide-f <67809479+davide-f@users.noreply.github.com>
2021-09-13 16:43:27 +0200

    Fixing issues

--- scripts/build_shapes.py
+++ scripts/build_shapes.py
@@ -271,7 +271,7 @@ def eez(countries, country_shapes, update=False, out_logging=False, tol=1e-3):
             
             geom = ret_df[selection].geometry.unary_union
             ret_df.drop(ret_df[selection].index, inplace=True)
-            ret_df.loc[ret_df.shape[0]] = [c_code, geom]
+            ret_df = ret_df.append({"name":c_code, "geometry": geom}, ignore_index=True)
 
     ret_df = ret_df.set_index("name")["geometry"].map(
         lambda x: _simplify_polys(x, minarea=0.001, tolerance=0.0001))
@@ -339,7 +339,7 @@ def eez_new(countries, country_shapes, out_logging=False, distance=0.01):
             
             geom = ret_df[selection].geometry.unary_union
             ret_df.drop(ret_df[selection].index, inplace=True)
-            ret_df.loc[ret_df.shape[0]] = [c_code, geom]
+            ret_df = ret_df.append({"name":c_code, "geometry": geom}, ignore_index=True)
 
     ret_df = ret_df.set_index("name")["geometry"].map(
         lambda x: _simplify_polys(x, minarea=0.001, tolerance=0.0001))
