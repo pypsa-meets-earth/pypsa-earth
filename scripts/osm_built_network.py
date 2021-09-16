2c3aa13a95b3276134e1b7a509deaa22bce2c281
Restyled.io <commits@restyled.io>
2021-09-16 18:25:48 +0000

    Restyled by isort

--- scripts/osm_built_network.py
+++ scripts/osm_built_network.py
@@ -5,9 +5,7 @@ import sys
 
 import geopandas as gpd
 import numpy as np
-from _helpers import _sets_path_to_root
-from _helpers import _to_csv_nafix
-from _helpers import configure_logging
+from _helpers import _sets_path_to_root, _to_csv_nafix, configure_logging
 
 logger = logging.getLogger(__name__)
 
