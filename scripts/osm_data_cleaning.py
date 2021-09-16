8c13f73ee29d5fb1029b11b0a76ace921fcb5ee0
Restyled.io <commits@restyled.io>
2021-09-16 18:25:48 +0000

    Restyled by black

--- scripts/osm_data_cleaning.py
+++ scripts/osm_data_cleaning.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from _helpers import _sets_path_to_root
 from _helpers import configure_logging
+
 # from shapely.geometry import LineString, Point, Polygon
 # from osm_data_config import AFRICA_CC
 
@@ -39,7 +40,8 @@ def prepare_substation_df(df_all_substations):
             "Country": "country",  # new/different to PyPSA-Eur
             "Area": "tag_area",
             "lonlat": "geometry",
-        })
+        }
+    )
 
     # Add longitute (lon) and latitude (lat) coordinates in the dataset
     df_all_substations["lon"] = df_all_substations["geometry"].x
@@ -116,11 +118,12 @@ def split_cells(df, lst_col="voltage"):
         Target column over which to perform the analysis
     """
     x = df.assign(**{lst_col: df[lst_col].str.split(";")})
-    x = pd.DataFrame({
-        col: np.repeat(x[col].values, x[lst_col].str.len())
-        for col in x.columns.difference([lst_col])
-    }).assign(
-        **{lst_col: np.concatenate(x[lst_col].values)})[x.columns.tolist()]
+    x = pd.DataFrame(
+        {
+            col: np.repeat(x[col].values, x[lst_col].str.len())
+            for col in x.columns.difference([lst_col])
+        }
+    ).assign(**{lst_col: np.concatenate(x[lst_col].values)})[x.columns.tolist()]
     return x
 
 
@@ -133,8 +136,9 @@ def filter_voltage(df, threshold_voltage=35000):
     df = split_cells(df)
 
     # Convert voltage to float, if impossible, discard row
-    df["voltage"] = (df["voltage"].apply(
-        lambda x: pd.to_numeric(x, errors="coerce")).astype(float))
+    df["voltage"] = (
+        df["voltage"].apply(lambda x: pd.to_numeric(x, errors="coerce")).astype(float)
+    )
     df = df.dropna(subset=["voltage"])  # Drop any row with Voltage = N/A
 
     # convert voltage to int
@@ -153,9 +157,7 @@ def finalize_substation_types(df_all_substations):
 
     # make float to integer
     df_all_substations["bus_id"] = df_all_substations["bus_id"].astype(int)
-    df_all_substations.loc[:,
-                           "voltage"] = df_all_substations["voltage"].astype(
-                               int)
+    df_all_substations.loc[:, "voltage"] = df_all_substations["voltage"].astype(int)
 
     return df_all_substations
 
@@ -182,7 +184,8 @@ def prepare_lines_df(df_lines):
             "lonlat": "geometry",
             "Country": "country",  # new/different to PyPSA-Eur
             "Length": "length",
-        })
+        }
+    )
 
     # Add NaN as default
     df_lines["bus0"] = np.nan
@@ -250,8 +253,9 @@ def integrate_lines_df(df_all_lines):
     # if not int make int
     if df_all_lines["cables"].dtype != int:
         # HERE. "0" if cables "None", "nan" or "1"
-        df_all_lines.loc[(df_all_lines["cables"] < "3")
-                         | df_all_lines["cables"].isna(), "cables"] = "0"
+        df_all_lines.loc[
+            (df_all_lines["cables"] < "3") | df_all_lines["cables"].isna(), "cables"
+        ] = "0"
         df_all_lines["cables"] = df_all_lines["cables"].astype("int")
 
     # downgrade 4 and 5 cables to 3...
@@ -259,17 +263,20 @@ def integrate_lines_df(df_all_lines):
         # Reason: 4 cables have 1 lighting protection cables, 5 cables has 2 LP cables - not transferring energy;
         # see https://hackaday.com/2019/06/11/a-field-guide-to-transmission-lines/
         # where circuits are "0" make "1"
-        df_all_lines.loc[(df_all_lines["cables"] == 4) |
-                         (df_all_lines["cables"] == 5), "cables"] = 3
+        df_all_lines.loc[
+            (df_all_lines["cables"] == 4) | (df_all_lines["cables"] == 5), "cables"
+        ] = 3
 
     # one circuit contains 3 cable
     df_all_lines.loc[df_all_lines["circuits"].isna(), "circuits"] = (
-        df_all_lines.loc[df_all_lines["circuits"].isna(), "cables"] / 3)
+        df_all_lines.loc[df_all_lines["circuits"].isna(), "cables"] / 3
+    )
     df_all_lines["circuits"] = df_all_lines["circuits"].astype(int)
 
     # where circuits are "0" make "1"
-    df_all_lines.loc[(df_all_lines["circuits"] == "0") |
-                     (df_all_lines["circuits"] == 0), "circuits"] = 1
+    df_all_lines.loc[
+        (df_all_lines["circuits"] == "0") | (df_all_lines["circuits"] == 0), "circuits"
+    ] = 1
 
     # drop column if exist
     if "cables" in df_all_lines:
@@ -288,39 +295,43 @@ def prepare_generators_df(df_all_generators):
 
     # rename columns
     df_all_generators = df_all_generators.rename(
-        columns={"tags.generator:output:electricity": "power_output_MW"})
+        columns={"tags.generator:output:electricity": "power_output_MW"}
+    )
 
     # convert electricity column from string to float value
     df_all_generators = df_all_generators[
-        df_all_generators["power_output_MW"].astype(str).str.contains("MW")]
+        df_all_generators["power_output_MW"].astype(str).str.contains("MW")
+    ]
     df_all_generators["power_output_MW"] = (
-        df_all_generators["power_output_MW"].str.extract("(\\d+)").astype(
-            float))
+        df_all_generators["power_output_MW"].str.extract("(\\d+)").astype(float)
+    )
 
     return df_all_generators
 
 
 def clean_data(tag_substation="transmission", threshold_voltage=35000):
     # Output file directory
-    outputfile_partial = os.path.join(os.getcwd(), "data", "clean",
-                                      "africa_all")
+    outputfile_partial = os.path.join(os.getcwd(), "data", "clean", "africa_all")
     # Output file directory
-    raw_outputfile_partial = os.path.join(os.getcwd(), "data", "raw",
-                                          "africa_all" + "_raw")
+    raw_outputfile_partial = os.path.join(
+        os.getcwd(), "data", "raw", "africa_all" + "_raw"
+    )
 
     # ----------- LINES AND CABLES -----------
 
     # Load raw data lines
-    df_lines = gpd.read_file(raw_outputfile_partial + "_lines" +
-                             ".geojson").set_crs(epsg=4326, inplace=True)
+    df_lines = gpd.read_file(raw_outputfile_partial + "_lines" + ".geojson").set_crs(
+        epsg=4326, inplace=True
+    )
 
     # prepare lines dataframe and data types
     df_lines = prepare_lines_df(df_lines)
     df_lines = finalize_lines_type(df_lines)
 
     # Load raw data lines
-    df_cables = gpd.read_file(raw_outputfile_partial + "_cables" +
-                              ".geojson").set_crs(epsg=4326, inplace=True)
+    df_cables = gpd.read_file(raw_outputfile_partial + "_cables" + ".geojson").set_crs(
+        epsg=4326, inplace=True
+    )
 
     # prepare cables dataframe and data types
     df_cables = prepare_lines_df(df_cables)
@@ -337,23 +348,21 @@ def clean_data(tag_substation="transmission", threshold_voltage=35000):
     df_all_lines = filter_voltage(df_all_lines, threshold_voltage)
 
     # remove lines without endings
-    df_all_lines = df_all_lines[df_all_lines["geometry"].map(
-        lambda g: len(g.boundary) >= 2)]
+    df_all_lines = df_all_lines[
+        df_all_lines["geometry"].map(lambda g: len(g.boundary) >= 2)
+    ]
 
     # set unique line ids
     df_all_lines = set_unique_id(df_all_lines, "line_id")
 
-    df_all_lines = gpd.GeoDataFrame(df_all_lines,
-                                    geometry="geometry",
-                                    crs="EPSG:4326")
-    df_all_lines.to_file(outputfile_partial + "_lines" + ".geojson",
-                         driver="GeoJSON")
+    df_all_lines = gpd.GeoDataFrame(df_all_lines, geometry="geometry", crs="EPSG:4326")
+    df_all_lines.to_file(outputfile_partial + "_lines" + ".geojson", driver="GeoJSON")
 
     # ----------- SUBSTATIONS -----------
 
-    df_all_substations = gpd.read_file(raw_outputfile_partial +
-                                       "_substations" + ".geojson").set_crs(
-                                           epsg=4326, inplace=True)
+    df_all_substations = gpd.read_file(
+        raw_outputfile_partial + "_substations" + ".geojson"
+    ).set_crs(epsg=4326, inplace=True)
 
     # prepare dataset for substations
     df_all_substations = prepare_substation_df(df_all_substations)
@@ -361,7 +370,8 @@ def clean_data(tag_substation="transmission", threshold_voltage=35000):
     # filter substations by tag
     if tag_substation:  # if the string is not empty check it
         df_all_substations = df_all_substations[
-            df_all_substations["tag_substation"] == tag_substation]
+            df_all_substations["tag_substation"] == tag_substation
+        ]
 
     # filter substation by voltage
     df_all_substations = filter_voltage(df_all_substations, threshold_voltage)
@@ -373,24 +383,25 @@ def clean_data(tag_substation="transmission", threshold_voltage=35000):
     df_all_substations = set_unique_id(df_all_substations, "bus_id")
 
     # save to geojson file
-    df_all_substations = gpd.GeoDataFrame(df_all_substations,
-                                          geometry="geometry",
-                                          crs="EPSG:4326")
-    df_all_substations.to_file(outputfile_partial + "_substations" +
-                               ".geojson",
-                               driver="GeoJSON")
+    df_all_substations = gpd.GeoDataFrame(
+        df_all_substations, geometry="geometry", crs="EPSG:4326"
+    )
+    df_all_substations.to_file(
+        outputfile_partial + "_substations" + ".geojson", driver="GeoJSON"
+    )
 
     # ----------- GENERATORS -----------
 
-    df_all_generators = gpd.read_file(raw_outputfile_partial + "_generators" +
-                                      ".geojson").set_crs(epsg=4326,
-                                                          inplace=True)
+    df_all_generators = gpd.read_file(
+        raw_outputfile_partial + "_generators" + ".geojson"
+    ).set_crs(epsg=4326, inplace=True)
 
     # prepare the generator dataset
     df_all_generators = prepare_generators_df(df_all_generators)
 
-    df_all_generators.to_file(outputfile_partial + "_generators" + ".geojson",
-                              driver="GeoJSON")
+    df_all_generators.to_file(
+        outputfile_partial + "_generators" + ".geojson", driver="GeoJSON"
+    )
 
     return None
 
@@ -405,10 +416,9 @@ if __name__ == "__main__":
     # Required to set path to pypsa-africa
     _sets_path_to_root("pypsa-africa")
 
-    tag_substation = snakemake.config["osm_data_cleaning_options"][
-        "tag_substation"]
+    tag_substation = snakemake.config["osm_data_cleaning_options"]["tag_substation"]
     threshold_voltage = snakemake.config["osm_data_cleaning_options"][
-        "threshold_voltage"]
+        "threshold_voltage"
+    ]
 
-    clean_data(tag_substation=tag_substation,
-               threshold_voltage=threshold_voltage)
+    clean_data(tag_substation=tag_substation, threshold_voltage=threshold_voltage)
