806eef73e9506e1d83b8c34a4164a1effbf53f40
Restyled.io <commits@restyled.io>
2021-10-05 14:58:06 +0000

    Restyled by black

--- scripts/build_renewable_profiles.py
+++ scripts/build_renewable_profiles.py
@@ -200,30 +200,30 @@ logger = logging.getLogger(__name__)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-if __name__ == '__main__':
-    if 'snakemake' not in globals():
+if __name__ == "__main__":
+    if "snakemake" not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake(
-            'build_renewable_profiles', technology='solar')
+
+        snakemake = mock_snakemake("build_renewable_profiles", technology="solar")
     configure_logging(snakemake)
     pgb.streams.wrap_stderr()
     paths = snakemake.input
-    nprocesses = snakemake.config['atlite'].get('nprocesses')
-    noprogress = not snakemake.config['atlite'].get('show_progress', True)
-    config = snakemake.config['renewable'][snakemake.wildcards.technology]
-    resource = config['resource']  # pv panel config / wind turbine config
-    correction_factor = config.get('correction_factor', 1.)
-    capacity_per_sqkm = config['capacity_per_sqkm']
-    p_nom_max_meth = config.get('potential', 'conservative')
+    nprocesses = snakemake.config["atlite"].get("nprocesses")
+    noprogress = not snakemake.config["atlite"].get("show_progress", True)
+    config = snakemake.config["renewable"][snakemake.wildcards.technology]
+    resource = config["resource"]  # pv panel config / wind turbine config
+    correction_factor = config.get("correction_factor", 1.0)
+    capacity_per_sqkm = config["capacity_per_sqkm"]
+    p_nom_max_meth = config.get("potential", "conservative")
 
     if isinstance(config.get("corine", {}), list):
-        config['corine'] = {'grid_codes': config['corine']}
+        config["corine"] = {"grid_codes": config["corine"]}
 
-    if correction_factor != 1.:
-        logger.info(f'correction_factor is set as {correction_factor}')
+    if correction_factor != 1.0:
+        logger.info(f"correction_factor is set as {correction_factor}")
 
-    cutout = atlite.Cutout(paths['cutout'])
-    regions = gpd.read_file(paths.regions).set_index('name').rename_axis('bus')
+    cutout = atlite.Cutout(paths["cutout"])
+    regions = gpd.read_file(paths.regions).set_index("name").rename_axis("bus")
     # TODO: Drop NAN maybe somewhere else? NaN lead to error otherwise
     regions = regions.dropna(axis="index", subset=["geometry"])
     buses = regions.index
@@ -259,45 +259,50 @@ if __name__ == '__main__':
 
     kwargs = dict(nprocesses=nprocesses, disable_progressbar=noprogress)
     if noprogress:
-        logger.info('Calculate landuse availabilities...')
+        logger.info("Calculate landuse availabilities...")
         start = time.time()
         availability = cutout.availabilitymatrix(regions, excluder, **kwargs)
         duration = time.time() - start
-        logger.info(f'Completed availability calculation ({duration:2.2f}s)')
+        logger.info(f"Completed availability calculation ({duration:2.2f}s)")
     else:
         availability = cutout.availabilitymatrix(regions, excluder, **kwargs)
 
     area = cutout.grid.to_crs(3035).area / 1e6
-    area = xr.DataArray(area.values.reshape(cutout.shape),
-                        [cutout.coords['y'], cutout.coords['x']])
+    area = xr.DataArray(
+        area.values.reshape(cutout.shape), [cutout.coords["y"], cutout.coords["x"]]
+    )
 
-    potential = capacity_per_sqkm * availability.sum('bus') * area
-    func = getattr(cutout, resource.pop('method'))
-    resource['dask_kwargs'] = {'num_workers': nprocesses}
-    capacity_factor = correction_factor * \
-        func(capacity_factor=True, **resource)
+    potential = capacity_per_sqkm * availability.sum("bus") * area
+    func = getattr(cutout, resource.pop("method"))
+    resource["dask_kwargs"] = {"num_workers": nprocesses}
+    capacity_factor = correction_factor * func(capacity_factor=True, **resource)
     layout = capacity_factor * area * capacity_per_sqkm
-    profile, capacities = func(matrix=availability.stack(spatial=['y', 'x']),
-                               layout=layout, index=buses,
-                               per_unit=True, return_capacity=True, **resource)
+    profile, capacities = func(
+        matrix=availability.stack(spatial=["y", "x"]),
+        layout=layout,
+        index=buses,
+        per_unit=True,
+        return_capacity=True,
+        **resource,
+    )
 
-    logger.info(
-        f"Calculating maximal capacity per bus (method '{p_nom_max_meth}')")
-    if p_nom_max_meth == 'simple':
+    logger.info(f"Calculating maximal capacity per bus (method '{p_nom_max_meth}')")
+    if p_nom_max_meth == "simple":
         p_nom_max = capacity_per_sqkm * availability @ area
-    elif p_nom_max_meth == 'conservative':
-        max_cap_factor = capacity_factor.where(
-            availability != 0).max(['x', 'y'])
+    elif p_nom_max_meth == "conservative":
+        max_cap_factor = capacity_factor.where(availability != 0).max(["x", "y"])
         p_nom_max = capacities / max_cap_factor
     else:
-        raise AssertionError('Config key `potential` should be one of "simple" '
-                             f'(default) or "conservative", not "{p_nom_max_meth}"')
+        raise AssertionError(
+            'Config key `potential` should be one of "simple" '
+            f'(default) or "conservative", not "{p_nom_max_meth}"'
+        )
 
-    logger.info('Calculate average distances.')
-    layoutmatrix = (layout * availability).stack(spatial=['y', 'x'])
+    logger.info("Calculate average distances.")
+    layoutmatrix = (layout * availability).stack(spatial=["y", "x"])
 
-    coords = cutout.grid[['x', 'y']]
-    bus_coords = regions[['x', 'y']]
+    coords = cutout.grid[["x", "y"]]
+    bus_coords = regions[["x", "y"]]
 
     average_distance = []
     centre_of_mass = []
@@ -306,19 +311,22 @@ if __name__ == '__main__':
         nz_b = row != 0
         row = row[nz_b]
         co = coords[nz_b]
-        distances = haversine(bus_coords.loc[bus],  co)
+        distances = haversine(bus_coords.loc[bus], co)
         average_distance.append((distances * (row / row.sum())).sum())
         centre_of_mass.append(co.values.T @ (row / row.sum()))
 
     average_distance = xr.DataArray(average_distance, [buses])
-    centre_of_mass = xr.DataArray(
-        centre_of_mass, [buses, ('spatial', ['x', 'y'])])
+    centre_of_mass = xr.DataArray(centre_of_mass, [buses, ("spatial", ["x", "y"])])
 
-    ds = xr.merge([(correction_factor * profile).rename('profile'),
-                   capacities.rename('weight'),
-                   p_nom_max.rename('p_nom_max'),
-                   potential.rename('potential'),
-                   average_distance.rename('average_distance')])
+    ds = xr.merge(
+        [
+            (correction_factor * profile).rename("profile"),
+            capacities.rename("weight"),
+            p_nom_max.rename("p_nom_max"),
+            potential.rename("potential"),
+            average_distance.rename("average_distance"),
+        ]
+    )
 
     # if snakemake.wildcards.technology.startswith("offwind"):
     #     logger.info('Calculate underwater fraction of connections.')
@@ -333,11 +341,15 @@ if __name__ == '__main__':
     #     ds['underwater_fraction'] = xr.DataArray(underwater_fraction, [buses])
 
     # select only buses with some capacity and minimal capacity factor
-    ds = ds.sel(bus=((ds['profile'].mean('time') > config.get('min_p_max_pu', 0.)) &
-                     (ds['p_nom_max'] > config.get('min_p_nom_max', 0.))))
+    ds = ds.sel(
+        bus=(
+            (ds["profile"].mean("time") > config.get("min_p_max_pu", 0.0))
+            & (ds["p_nom_max"] > config.get("min_p_nom_max", 0.0))
+        )
+    )
 
-    if 'clip_p_max_pu' in config:
-        min_p_max_pu = config['clip_p_max_pu']
-        ds['profile'] = ds['profile'].where(ds['profile'] >= min_p_max_pu, 0)
+    if "clip_p_max_pu" in config:
+        min_p_max_pu = config["clip_p_max_pu"]
+        ds["profile"] = ds["profile"].where(ds["profile"] >= min_p_max_pu, 0)
 
     ds.to_netcdf(snakemake.output.profile)
--- scripts/simplify_network.py
+++ scripts/simplify_network.py
@@ -100,6 +100,7 @@ from pypsa.networkclustering import aggregateoneport
 from pypsa.networkclustering import busmap_by_stubs
 from scipy.sparse.csgraph import connected_components
 from scipy.sparse.csgraph import dijkstra
+
 sys.settrace
 
 logger = logging.getLogger(__name__)
@@ -114,26 +115,25 @@ def simplify_network_to_380(n):
 
     n.buses["v_nom"] = 380.0
 
-    (linetype_380, ) = n.lines.loc[n.lines.v_nom == 380.0, "type"].unique()
+    (linetype_380,) = n.lines.loc[n.lines.v_nom == 380.0, "type"].unique()
     lines_v_nom_b = n.lines.v_nom != 380.0
-    n.lines.loc[lines_v_nom_b,
-                "num_parallel"] *= (n.lines.loc[lines_v_nom_b, "v_nom"] /
-                                    380.0)**2
+    n.lines.loc[lines_v_nom_b, "num_parallel"] *= (
+        n.lines.loc[lines_v_nom_b, "v_nom"] / 380.0
+    ) ** 2
     n.lines.loc[lines_v_nom_b, "v_nom"] = 380.0
     n.lines.loc[lines_v_nom_b, "type"] = linetype_380
-    n.lines.loc[lines_v_nom_b,
-                "s_nom"] = (np.sqrt(3) *
-                            n.lines["type"].map(n.line_types.i_nom) *
-                            n.lines.bus0.map(n.buses.v_nom) *
-                            n.lines.num_parallel)
+    n.lines.loc[lines_v_nom_b, "s_nom"] = (
+        np.sqrt(3)
+        * n.lines["type"].map(n.line_types.i_nom)
+        * n.lines.bus0.map(n.buses.v_nom)
+        * n.lines.num_parallel
+    )
 
     # Replace transformers by lines
-    trafo_map = pd.Series(n.transformers.bus1.values,
-                          index=n.transformers.bus0.values)
+    trafo_map = pd.Series(n.transformers.bus1.values, index=n.transformers.bus0.values)
     trafo_map = trafo_map[~trafo_map.index.duplicated(keep="first")]
     several_trafo_b = trafo_map.isin(trafo_map.index)
-    trafo_map.loc[several_trafo_b] = trafo_map.loc[several_trafo_b].map(
-        trafo_map)
+    trafo_map.loc[several_trafo_b] = trafo_map.loc[several_trafo_b].map(trafo_map)
     missing_buses_i = n.buses.index.difference(trafo_map.index)
     trafo_map = trafo_map.append(pd.Series(missing_buses_i, missing_buses_i))
 
@@ -165,19 +165,22 @@ def _prepare_connection_costs_per_link(n):
     for tech in snakemake.config["renewable"]:
         if tech.startswith("offwind"):
             connection_costs_per_link[tech] = (
-                n.links.length * snakemake.config["lines"]["length_factor"] *
-                (n.links.underwater_fraction *
-                 costs.at[tech + "-connection-submarine", "capital_cost"] +
-                 (1.0 - n.links.underwater_fraction) *
-                 costs.at[tech + "-connection-underground", "capital_cost"]))
+                n.links.length
+                * snakemake.config["lines"]["length_factor"]
+                * (
+                    n.links.underwater_fraction
+                    * costs.at[tech + "-connection-submarine", "capital_cost"]
+                    + (1.0 - n.links.underwater_fraction)
+                    * costs.at[tech + "-connection-underground", "capital_cost"]
+                )
+            )
 
     return connection_costs_per_link
 
 
-def _compute_connection_costs_to_bus(n,
-                                     busmap,
-                                     connection_costs_per_link=None,
-                                     buses=None):
+def _compute_connection_costs_to_bus(
+    n, busmap, connection_costs_per_link=None, buses=None
+):
     if connection_costs_per_link is None:
         connection_costs_per_link = _prepare_connection_costs_per_link(n)
 
@@ -187,17 +190,21 @@ def _compute_connection_costs_to_bus(n,
     connection_costs_to_bus = pd.DataFrame(index=buses)
 
     for tech in connection_costs_per_link:
-        adj = n.adjacency_matrix(weights=pd.concat(
-            dict(
-                Link=connection_costs_per_link[tech].reindex(n.links.index),
-                Line=pd.Series(0.0, n.lines.index),
-            )))
+        adj = n.adjacency_matrix(
+            weights=pd.concat(
+                dict(
+                    Link=connection_costs_per_link[tech].reindex(n.links.index),
+                    Line=pd.Series(0.0, n.lines.index),
+                )
+            )
+        )
 
         costs_between_buses = dijkstra(
-            adj, directed=False, indices=n.buses.index.get_indexer(buses))
+            adj, directed=False, indices=n.buses.index.get_indexer(buses)
+        )
         connection_costs_to_bus[tech] = costs_between_buses[
-            np.arange(len(buses)),
-            n.buses.index.get_indexer(busmap.loc[buses])]
+            np.arange(len(buses)), n.buses.index.get_indexer(busmap.loc[buses])
+        ]
 
     return connection_costs_to_bus
 
@@ -205,24 +212,27 @@ def _compute_connection_costs_to_bus(n,
 def _adjust_capital_costs_using_connection_costs(n, connection_costs_to_bus):
     for tech in connection_costs_to_bus:
         tech_b = n.generators.carrier == tech
-        costs = (n.generators.loc[tech_b, "bus"].map(
-            connection_costs_to_bus[tech]).loc[lambda s: s > 0])
+        costs = (
+            n.generators.loc[tech_b, "bus"]
+            .map(connection_costs_to_bus[tech])
+            .loc[lambda s: s > 0]
+        )
         if not costs.empty:
             n.generators.loc[costs.index, "capital_cost"] += costs
             logger.info(
-                "Displacing {} generator(s) and adding connection costs to capital_costs: {} "
-                .format(
+                "Displacing {} generator(s) and adding connection costs to capital_costs: {} ".format(
                     tech,
-                    ", ".join("{:.0f} Eur/MW/a for `{}`".format(d, b)
-                              for b, d in costs.iteritems()),
-                ))
+                    ", ".join(
+                        "{:.0f} Eur/MW/a for `{}`".format(d, b)
+                        for b, d in costs.iteritems()
+                    ),
+                )
+            )
 
 
-def _aggregate_and_move_components(n,
-                                   busmap,
-                                   connection_costs_to_bus,
-                                   aggregate_one_ports={"Load",
-                                                        "StorageUnit"}):
+def _aggregate_and_move_components(
+    n, busmap, connection_costs_to_bus, aggregate_one_ports={"Load", "StorageUnit"}
+):
     def replace_components(n, c, df, pnl):
         n.mremove(c, n.df(c).index)
 
@@ -234,7 +244,8 @@ def _aggregate_and_move_components(n,
     _adjust_capital_costs_using_connection_costs(n, connection_costs_to_bus)
 
     generators, generators_pnl = aggregategenerators(
-        n, busmap, custom_strategies={"p_nom_min": np.sum})
+        n, busmap, custom_strategies={"p_nom_min": np.sum}
+    )
     replace_components(n, "Generator", generators, generators_pnl)
 
     for one_port in aggregate_one_ports:
@@ -245,9 +256,7 @@ def _aggregate_and_move_components(n,
     n.mremove("Bus", buses_to_del)
     for c in n.branch_components:
         df = n.df(c)
-        n.mremove(
-            c,
-            df.index[df.bus0.isin(buses_to_del) | df.bus1.isin(buses_to_del)])
+        n.mremove(c, df.index[df.bus0.isin(buses_to_del) | df.bus1.isin(buses_to_del)])
 
 
 def simplify_links(n):
@@ -272,10 +281,7 @@ def simplify_links(n):
         nodes = frozenset(nodes)
 
         seen = set()
-        supernodes = {
-            m
-            for m in nodes if len(G.adj[m]) > 2 or (set(G.adj[m]) - nodes)
-        }
+        supernodes = {m for m in nodes if len(G.adj[m]) > 2 or (set(G.adj[m]) - nodes)}
 
         for u in supernodes:
             for m, ls in G.adj[u].items():
@@ -305,7 +311,8 @@ def simplify_links(n):
 
     connection_costs_per_link = _prepare_connection_costs_per_link(n)
     connection_costs_to_bus = pd.DataFrame(
-        0.0, index=n.buses.index, columns=list(connection_costs_per_link))
+        0.0, index=n.buses.index, columns=list(connection_costs_per_link)
+    )
 
     for lbl in labels.value_counts().loc[lambda s: s > 2].index:
 
@@ -314,16 +321,15 @@ def simplify_links(n):
                 continue
 
             logger.debug("nodes = {}".format(labels.index[labels == lbl]))
-            logger.debug("b = {}\nbuses = {}\nlinks = {}".format(
-                b, buses, links))
+            logger.debug("b = {}\nbuses = {}\nlinks = {}".format(b, buses, links))
 
             m = sp.spatial.distance_matrix(
-                n.buses.loc[b, ["x", "y"]], n.buses.loc[buses[1:-1],
-                                                        ["x", "y"]])
+                n.buses.loc[b, ["x", "y"]], n.buses.loc[buses[1:-1], ["x", "y"]]
+            )
             busmap.loc[buses] = b[np.r_[0, m.argmin(axis=0), 1]]
-            connection_costs_to_bus.loc[
-                buses] += _compute_connection_costs_to_bus(
-                    n, busmap, connection_costs_per_link, buses)
+            connection_costs_to_bus.loc[buses] += _compute_connection_costs_to_bus(
+                n, busmap, connection_costs_per_link, buses
+            )
 
             all_links = [i for _, i in sum(links, [])]
 
@@ -334,13 +340,15 @@ def simplify_links(n):
                 carrier="DC",
                 bus0=b[0],
                 bus1=b[1],
-                length=sum(n.links.loc[[i for _, i in l], "length"].mean()
-                           for l in links),
-                p_nom=min(n.links.loc[[i for _, i in l], "p_nom"].sum()
-                          for l in links),
+                length=sum(
+                    n.links.loc[[i for _, i in l], "length"].mean() for l in links
+                ),
+                p_nom=min(n.links.loc[[i for _, i in l], "p_nom"].sum() for l in links),
                 underwater_fraction=sum(
-                    lengths / lengths.sum() *
-                    n.links.loc[all_links, "underwater_fraction"]),
+                    lengths
+                    / lengths.sum()
+                    * n.links.loc[all_links, "underwater_fraction"]
+                ),
                 p_max_pu=p_max_pu,
                 p_min_pu=-p_max_pu,
                 underground=False,
@@ -348,13 +356,14 @@ def simplify_links(n):
             )
 
             logger.info(
-                "Joining the links {} connecting the buses {} to simple link {}"
-                .format(", ".join(all_links), ", ".join(buses), name))
+                "Joining the links {} connecting the buses {} to simple link {}".format(
+                    ", ".join(all_links), ", ".join(buses), name
+                )
+            )
 
             n.mremove("Link", all_links)
 
-            static_attrs = n.components["Link"]["attrs"].loc[
-                lambda df: df.static]
+            static_attrs = n.components["Link"]["attrs"].loc[lambda df: df.static]
             for attr, default in static_attrs.default.iteritems():
                 params.setdefault(attr, default)
             n.links.loc[name] = pd.Series(params)
@@ -384,22 +393,33 @@ def cluster(n, n_clusters):
 
     focus_weights = snakemake.config.get("focus_weights", None)
 
-    renewable_carriers = pd.Index([
-        tech for tech in n.generators.carrier.unique()
-        if tech.split("-", 2)[0] in snakemake.config["renewable"]
-    ])
+    renewable_carriers = pd.Index(
+        [
+            tech
+            for tech in n.generators.carrier.unique()
+            if tech.split("-", 2)[0] in snakemake.config["renewable"]
+        ]
+    )
 
     def consense(x):
         v = x.iat[0]
-        assert (x == v).all() or x.isnull().all(
-        ), "The `potential` configuration option must agree for all renewable carriers, for now!"
+        assert (
+            x == v
+        ).all() or x.isnull().all(), "The `potential` configuration option must agree for all renewable carriers, for now!"
         return v
 
-    potential_mode = (consense(
-        pd.Series([
-            snakemake.config["renewable"][tech]["potential"]
-            for tech in renewable_carriers
-        ])) if len(renewable_carriers) > 0 else "conservative")
+    potential_mode = (
+        consense(
+            pd.Series(
+                [
+                    snakemake.config["renewable"][tech]["potential"]
+                    for tech in renewable_carriers
+                ]
+            )
+        )
+        if len(renewable_carriers) > 0
+        else "conservative"
+    )
     clustering = clustering_for_n_clusters(
         n,
         n_clusters,
@@ -416,23 +436,24 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("simplify_network",
-                                   simpl="",
-                                   network="elec")
+        snakemake = mock_snakemake("simplify_network", simpl="", network="elec")
     configure_logging(snakemake)
 
     n = pypsa.Network(snakemake.input.network)
 
     # Add dummy load data                                                        #TODO remove
-    n.madd("Load", ['load ' + s for s in list(n.buses.index)],
-           bus=list(n.buses.index),
-           p_set=np.random.rand(len(n.snapshots), len(n.buses)))
+    n.madd(
+        "Load",
+        ["load " + s for s in list(n.buses.index)],
+        bus=list(n.buses.index),
+        p_set=np.random.rand(len(n.snapshots), len(n.buses)),
+    )
 
     n, trafo_map = simplify_network_to_380(n)
 
     n, simplify_links_map = simplify_links(n)
 
-    #n, stub_map = remove_stubs(n)
+    # n, stub_map = remove_stubs(n)
 
     busmaps = [trafo_map, simplify_links_map]  # , stub_map]
 
@@ -442,7 +463,8 @@ if __name__ == "__main__":
     else:
         # TODO: Remove other unnecessary columns
         n.buses = n.buses.drop(
-            ['symbol', 'under_construction', 'substation_lv', 'substation_off'], axis=1)
+            ["symbol", "under_construction", "substation_lv", "substation_off"], axis=1
+        )
 
     update_p_nom_max(n)
 
