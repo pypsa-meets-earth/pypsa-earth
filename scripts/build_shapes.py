ead487f3baf16a86217f18192d7f93900410183d
davide-f <fioritidavidesubs@gmail.com>
2021-12-06 18:31:29 +0100

    Revert decommenting build_shapes

--- scripts/build_shapes.py
+++ scripts/build_shapes.py
@@ -759,17 +759,17 @@ def gadm(countries, layer_id=2, update=False, out_logging=False, year=2020, npro
     # drop useless columns
     df_gadm = df_gadm[["country", "GADM_ID", "geometry"]]
 
-    # # add the population data to the dataset
-    # add_population_data(df_gadm, countries, year, update, out_logging, nprocesses=nprocesses)
+    # add the population data to the dataset
+    add_population_data(df_gadm, countries, year, update, out_logging, nprocesses=nprocesses)
 
-    # # add the gdp data to the dataset
-    # add_gdp_data(
-    #     df_gadm,
-    #     year,
-    #     update,
-    #     out_logging,
-    #     name_file_nc="GDP_PPP_1990_2015_5arcmin_v2.nc",
-    # )
+    # add the gdp data to the dataset
+    add_gdp_data(
+        df_gadm,
+        year,
+        update,
+        out_logging,
+        name_file_nc="GDP_PPP_1990_2015_5arcmin_v2.nc",
+    )
 
     # set index and simplify polygons
     df_gadm.set_index("GADM_ID", inplace=True)
