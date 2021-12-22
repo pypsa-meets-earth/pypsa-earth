8ee9982536dbd376e74d70b15808dbd0cd399958
pz-max <max.parzen@ed.ac.uk>
2021-12-22 18:24:32 +0100

    fix typo

--- scripts/add_electricity.py
+++ scripts/add_electricity.py
@@ -701,7 +701,7 @@ if __name__ == "__main__":
     configure_logging(snakemake)
 
     n = pypsa.Network(snakemake.input.base_network)
-    Nyears = n.snapshot_weightings.objective.sum().values[0] / 8760.0
+    Nyears = n.snapshot_weightings.objective.sum() / 8760.0
 
     # Snakemake imports:
     regions = snakemake.input.regions
