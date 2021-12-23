042ae50c9c617f5d946166c410823e34141220f0
Restyled.io <commits@restyled.io>
2021-12-23 16:48:08 +0000

    Restyled by yapf

--- scripts/osm_data_config.py
+++ scripts/osm_data_config.py
@@ -249,7 +249,8 @@ world = {
         # 'LA': "lao people's democratic republic",
         "LB": "lebanon",
         "MO": "macao",
-        "MY-SG-BN": "malaysia-singapore-brunei",  # Note 3 countries based on geofabrik
+        "MY-SG-BN":
+        "malaysia-singapore-brunei",  # Note 3 countries based on geofabrik
         "MV": "maldives",
         "MN": "mongolia",
         "MM": "myanmar",
@@ -380,8 +381,7 @@ world = {
 
 world_countries = {
     country_2D: country_name
-    for d in world.values()
-    for (country_2D, country_name) in d.items()
+    for d in world.values() for (country_2D, country_name) in d.items()
 }
 
 continent_regions = {
