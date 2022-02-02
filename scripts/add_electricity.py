bae8271e25608bf027dabe265872d398fe943d8d
Restyled.io <commits@restyled.io>
2022-02-02 23:46:27 +0000

    Restyled by isort

--- scripts/add_electricity.py
+++ scripts/add_electricity.py
@@ -90,8 +90,7 @@ import pandas as pd
 import powerplantmatching as pm
 import pypsa
 import xarray as xr
-from _helpers import configure_logging
-from _helpers import update_p_nom_max
+from _helpers import configure_logging, update_p_nom_max
 from powerplantmatching.export import map_country_bus
 from shapely.validation import make_valid
 from vresutils import transfer as vtransfer
