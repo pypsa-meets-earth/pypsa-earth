0f36d74b58ce94811af4906e76f778a4f05b617d
user <user@DEBIAN.local>
2022-02-04 11:50:39 +0100

    added morocco built networks

--- scripts/prepare_sector_networks.py
+++ scripts/prepare_sector_networks.py
@@ -1,1 +1,12 @@
+import pypsa
 
+
+
+add_generation(n, costs):
+    print(adding electricity generation)
+
+if __name__ == "__main__":
+
+    n = pypsa.Network("networks/elec_s_4.nc")
+
+    n.buses()
