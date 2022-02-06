403a0bab0695e0b419e46f825485cbd29fb1a18b
davide-f <fioritidavidesubs@gmail.com>
2022-02-07 00:12:42 +0100

    First implementation of flexible bundles specified by config.yaml

--- scripts/retrieve_databundle_light.py
+++ scripts/retrieve_databundle_light.py
@@ -34,6 +34,7 @@ import logging
 import os
 import tarfile
 from pathlib import Path
+from numpy import False_
 import yaml
 from zipfile import ZipFile
 
@@ -47,59 +48,80 @@ logger = logging.getLogger(__name__)
 
 def load_databundle_config(path):
     "Load databundle configurations from path file"
-
-    config = yaml.load(path, Loader=yaml.FullLoader)
+    with open(path) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
 
     # parse the "countries" list specified in the file before processing
     for bundle_name in config:
-        config["countries"] = create_country_list(config["countries"])
+        config[bundle_name]["countries"] = create_country_list(config[bundle_name]["countries"])
     
     return config
 
-def download_and_unzip(host, resource, url, file_path, remove_file=True):
+def download_and_unzip(host, config, rootpath):
     """
     Function to download and unzip data depending on the hosting platform.
     Currently, hosts accepted: zenodo and google
     """
+    resource="-".join(config["category"])
+    file_path=Path(rootpath, "tempfile.zip")
 
     if host=="zenodo":
-        progress_retrieve(url, file_path)
-        logger.info(f"Extracting resources")
-        with ZipFile(file_path, "r") as zipObj:
-            # Extract all the contents of zip file in current directory
-            zipObj.extractall()
-        if remove_file:
-            os.remove(file_path)
-        logger.info(f"Download data to '{destination}' from cloud '{url}'.")
+        url=config["urls"]["zenodo"]
+        # progress_retrieve(url, file_path)
+        # logger.info(f"Extracting resources")
+        # with ZipFile(file_path, "r") as zipObj:
+        #     # Extract all the contents of zip file in current directory
+        #     zipObj.extractall()
+        # os.remove(file_path)
+        logger.info(f"Download resource '{resource}' from cloud '{url}'.")
+        return True
     elif host=="google":
 
+        url=config["urls"]["google"]
         # retrieve file_id from path
         partition_view = str(url).split("/view", 1)  # cut the part before the ending \view
         if len(partition_view) < 2:
             logger.error(f"Resource {resource} cannot be downloaded: \"\\view\" not found in url {url}")
-            return
+            return False
         
         code_split = partition_view[0].rsplit("\\", 1)  # split url to get the file_id
 
         if len(partition_view) < 2:
             logger.error(f"Resource {resource} cannot be downloaded: character \"\\\" not found in {partition_view[0]}")
-            return
+            return False
         
         # get file id
         file_id = code_split[1]
 
-        gdd.download_file_from_google_drive(
-            file_id=file_id,
-            dest_path=file_path,
-            showsize=True,
-            unzip=True,
-        )
-        if remove_file:
-            os.remove(file_path)
-        logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-        
+        # gdd.download_file_from_google_drive(
+        #     file_id=file_id,
+        #     dest_path=file_path,
+        #     showsize=True,
+        #     unzip=True,
+        # )
+        # os.remove(file_path)
+        logger.info(f"Download resource '{resource}' from cloud '{url}'.")
+
+        return True
     else:
         logger.error(f"Host {host} not implemented")
+        return False
+
+def get_best_bundle(category, config_bundles, tutorial):
+    # dictionary with the number of match by configuration for tutorial/non-tutorial configurations
+    dict_n_matched = {bname:config_bundles[bname]["n_matched"] for bname in config_bundles
+        if config_bundles[bname]["category"] == category and config_bundles[bname].get("tutorial", False) == tutorial
+    }
+
+    # check if non-empty dictionary
+    if dict_n_matched:
+        # if non-empty, then pick the first bundle
+        dict_sort = sorted(dict_n_matched.items(), key=lambda d: d[1])
+
+        dname_download = dict_sort[0][0]
+        return [dname_download]
+    else:
+        return []
 
 if __name__ == "__main__":
     if "snakemake" not in globals():
@@ -112,8 +134,6 @@ if __name__ == "__main__":
 
     _sets_path_to_root("pypsa-africa")
     
-    # resources to download
-    resources_to_download=["resources", "data", "cutouts"]
 
     rootpath = os.getcwd()
     tutorial = snakemake.config["tutorial"]
@@ -122,155 +142,45 @@ if __name__ == "__main__":
     logger.info(f"Retrieving data from {host}.")
 
     # load databundle configuration
-    config_bundles = load_databundle_config(snakemake.input)
+    config_bundles = load_databundle_config(snakemake.input[0])
 
-    # list of bundles to download
-    bundles_to_download = []
+    # categories of data to download
+    categories = list(set([config_bundles[conf]["category"] for conf in config_bundles]))
 
-    # dictionary of best data
-
+    # idenfify matched countries for every bundle
     for bname in config_bundles:
-        # idenfify matched countries for every bundle
         config_bundles[bname]["matched_countries"] = [
-            c for c in config_bundles["countries"] if c in countries]
+            c for c in config_bundles[bname]["countries"] if c in countries]
         n_matched = len(config_bundles[bname]["matched_countries"])
         config_bundles[bname]["n_matched"] = n_matched
 
-        # if countries are matched
-        if n_matched > 0:
-            # when the data is necessary, append to the list of bundles to download
-            if config_bundles[bname].get("necessary", False):
-                bundles_to_download.append(n_matched)
+    # bundles to download
+    bundle_to_download = []
 
+    for cat in categories:
+        if tutorial:
+            selection_bundles = get_best_bundle(cat, config_bundles, tutorial)
 
-    if host == "zenodo":
-        # BUNDLE 1
-        destination = Path(f"{rootpath}/resources")
-        file_path = Path(f"{rootpath}/resources.zip")
-        url = "https://zenodo.org/record/5894972/files/resources.zip"
-        progress_retrieve(url, file_path)
-        logger.info(f"Extracting resources")
-        with ZipFile(file_path, "r") as zipObj:
-            # Extract all the contents of zip file in current directory
-            zipObj.extractall()
-        os.remove(file_path)
-        logger.info(f"Download data to '{destination}' from cloud '{url}'.")
+            # check if non-empty dictionary
+            if selection_bundles:
+                # if non-empty, then 
+                bundle_to_download.extend(selection_bundles)
+                continue
+            else:
+                logger.info(f"Tutorial data for {cat} not found, fall back to non-tutorial data")
 
-        if tutorial == True:
-            # BUNDLE 2
-            destination = Path(f"{rootpath}/data")
-            file_path = Path(f"{rootpath}/data-tutorial.zip")
-            url = "https://zenodo.org/record/5895010/files/data-tutorial.zip"
-            progress_retrieve(url, file_path)
-            logger.info(f"Extracting data")
-            with ZipFile(file_path, "r") as zipObj:
-                zipObj.extractall()
-            os.remove(file_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
+        
+        selection_bundles = get_best_bundle(cat, config_bundles, False)
 
-            # BUNDLE 3
-            destination = Path(f"{rootpath}/cutouts")
-            file_path = Path(f"{rootpath}/cutouts/africa-2013-era5-tutorial.nc")
-            url = "https://zenodo.org/record/5894926/files/africa-2013-era5-tutorial.nc"
-            progress_retrieve(url, file_path)
-            logger.info(f"Download cutouts to '{destination}' from cloud '{url}'.")
-
-        elif tutorial == False:
-            # BUNDLE 2
-            destination = Path(f"{rootpath}/data")
-            file_path = Path(f"{rootpath}/data.zip")
-            url = "https://zenodo.org/record/5895010/files/data.zip"
-            progress_retrieve(url, file_path)
-            logger.info(f"Extracting data")
-            with ZipFile(file_path, "r") as zipObj:
-                zipObj.extractall()
-            os.remove(file_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-            # BUNDLE 3
-            destination = Path(f"{rootpath}/cutouts")
-            file_path = Path(f"{rootpath}/cutouts/africa-2013-era5.nc")
-            url = "https://zenodo.org/record/5894926/files/africa-2013-era5.nc"
-            progress_retrieve(url, file_path)
-            logger.info(f"Download cutouts to '{destination}' from cloud '{url}'.")
-
-    if host == "google":
-        if tutorial == False:
-            # BUNDLE 1
-            destination = "./resources"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1hklbiRLyb_rx6WTvgCJ1Dx_9rooA85y1/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1hklbiRLyb_rx6WTvgCJ1Dx_9rooA85y1",
-                dest_path=zip_path,
-                showsize=True,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-            # BUNDLE 2
-            destination = "./data"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1IfSofV2PWUkAD_7yY-Xqv1X4duma2NkJ/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1IfSofV2PWUkAD_7yY-Xqv1X4duma2NkJ",
-                dest_path=zip_path,
-                showsize=True,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-            # BUNDLE 3
-            destination = "./cutouts"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1kyOH8wxm_cvnS7OoahCrFFVP-U7kWr_O/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1kyOH8wxm_cvnS7OoahCrFFVP-U7kWr_O",
-                dest_path=zip_path,
-                showsize=True,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-        if tutorial == True:
-            # BUNDLE 1
-            destination = "./resources"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1he31BBLtdemZt2dmBOwUCbP_jVuI3KS8/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1he31BBLtdemZt2dmBOwUCbP_jVuI3KS8",
-                dest_path=zip_path,
-                showsize=False,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-            # BUNDLE 2
-            destination = "./data"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1Jv4UMw7CoinZwIzl5nm1Z5oIuK7dG7gu/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1Jv4UMw7CoinZwIzl5nm1Z5oIuK7dG7gu",
-                dest_path=zip_path,
-                showsize=False,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
-
-            # BUNDLE 3
-            destination = "./cutouts"
-            zip_path = destination + ".zip"
-            url = "https://drive.google.com/file/d/1-Njs7BqG0YE5QwBHj0zgkdicb5IQvQCh/view?usp=sharing"
-            gdd.download_file_from_google_drive(
-                file_id="1-Njs7BqG0YE5QwBHj0zgkdicb5IQvQCh",
-                dest_path=zip_path,
-                showsize=False,
-                unzip=True,
-            )
-            os.remove(zip_path)
-            logger.info(f"Download data to '{destination}' from cloud '{url}'.")
+        # check if non-empty dictionary
+        if selection_bundles:
+            # if non-empty, then 
+            bundle_to_download.extend(selection_bundles)
+    
+    # download the selected bundles
+    for b_name in bundle_to_download:
+        host_list = config_bundles[b_name]["urls"]
+        # loop all hosts until data is successfully downloaded
+        for host in host_list:
+            if download_and_unzip(host, config_bundles[b_name], rootpath):
+                break
