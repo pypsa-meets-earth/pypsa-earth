4af482a3fa4b3d40fe82ce7b46281fd3de10e79b
user <hazem.abdel-khalek@ieg.fraunhofer.de>
2022-02-08 13:48:06 +0100

    added basic structure to the main func of prepare_sector_network

--- scripts/prepare_sector_networks.py
+++ scripts/prepare_sector_networks.py
@@ -1,12 +1,29 @@
 import pypsa
-
-
-
-add_generation(n, costs):
-    print(adding electricity generation)
+import os
 
 if __name__ == "__main__":
 
-    n = pypsa.Network("networks/elec_s_4.nc")
+    # TODO add mock_snakemake func
 
-    n.buses()
+    os.path.dirname(os.getcwd())
+    # TODO fetch from config
+    n = pypsa.Network(
+        "{}/networks/elec_s_4.nc".format(os.path.dirname(os.getcwd())))
+
+    n.buses
+
+    # TODO logging
+
+    # TODO fetch options from the config file
+
+    # TODO define spatial (for biomass and co2)
+
+    # TODO changes in case of myopic oversight
+
+    # TODO add co2 tracking function 
+
+    # TODO add generation
+
+    # TODO add storage  HERE THE H2 CARRIER IS ADDED IN PYPSA-EUR-SEC
+
+    # TODO add options as in PyPSA-EUR-SEC
