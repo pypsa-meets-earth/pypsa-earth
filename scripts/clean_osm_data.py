bd8ba3c797afe381275af98267f987e506eb97ea
Ekaterina Fedotova <e.v.kasilova@gmail.com>
2022-03-22 17:51:31 +0300

    Fix possible non-integer circuits formats

--- scripts/clean_osm_data.py
+++ scripts/clean_osm_data.py
@@ -347,8 +347,21 @@ def integrate_lines_df(df_all_lines):
         df_all_lines.loc[df_all_lines["circuits"].isna(), "cables"] / 3)
 
     # where circuits are "0" make "1"
-    df_all_lines.loc[(df_all_lines["circuits"] == "0") |
-                     (df_all_lines["circuits"] == 0), "circuits"] = 1
+    df_all_lines.loc[(df_all_lines["circuits"] == "0")
+                     # e.g. that's the case for Germany 
+                     |(df_all_lines["circuits"] == "0.5")
+                     # e.g. in the GB lines data
+                     |(df_all_lines["circuits"] == "d")
+                     # e.g. in the GB lines data
+                     |(df_all_lines["circuits"] == "`")
+                     # e.g. in the FR data
+                     |(df_all_lines["circuits"] == "1.")
+                     # e.g. in the PT data
+                     |(df_all_lines["circuits"] == "^1")
+                     # e.g. in the NO data
+                     |(df_all_lines["circuits"] == "partial")
+                     |(df_all_lines["circuits"] == 0), "circuits"] = 1  
+                     
     if df_all_lines["circuits"].dtype != int:
 
         # it's possible that df_all_lines["circuits"] == "1/3"
