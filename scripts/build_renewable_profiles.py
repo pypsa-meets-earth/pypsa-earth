7ea42002cbcb13670cabc2666b2c0b3a81116848
davide-f <fioritidavidesubs@gmail.com>
2022-05-30 16:38:08 +0200

    Revise build_renewable_profiles crs

--- scripts/build_renewable_profiles.py
+++ scripts/build_renewable_profiles.py
@@ -205,6 +205,9 @@ from shapely.ops import unary_union
 
 logger = logging.getLogger(__name__)
 
+COPERNICUS_CRS = "EPSG:4326"
+GEBCO_CRS = "EPSG:4326"
+
 if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
@@ -222,6 +225,8 @@ if __name__ == "__main__":
     resource = config["resource"]
     correction_factor = config.get("correction_factor", 1.0)
     p_nom_max_meth = config.get("potential", "conservative")
+    default_crs = snakemake.config["crs"]["default_crs"]
+    metric_crs = snakemake.config["crs"]["metric_crs"]
 
     if isinstance(config.get("copernicus", {}), list):
         config["copernicus"] = {"grid_codes": config["copernicus"]}
@@ -292,7 +297,7 @@ if __name__ == "__main__":
 
         capacity_per_sqkm = config["capacity_per_sqkm"]
 
-        excluder = atlite.ExclusionContainer(crs=3035, res=100)
+        excluder = atlite.ExclusionContainer(crs=metric_crs, res=100)
 
         if "natura" in config and config["natura"]:
             excluder.add_raster(paths.natura, nodata=0, allow_no_overlap=True)
@@ -303,14 +308,14 @@ if __name__ == "__main__":
                 paths.copernicus,
                 codes=copernicus["grid_codes"],
                 invert=True,
-                crs="EPSG:4326",
+                crs=COPERNICUS_CRS,
             )
             if "distance" in copernicus and config["copernicus"]["distance"] > 0:
                 excluder.add_raster(
                     paths.copernicus,
                     codes=copernicus["distance_grid_codes"],
                     buffer=copernicus["distance"],
-                    crs="EPSG:4326",
+                    crs=COPERNICUS_CRS,
                 )
 
         if "max_depth" in config:
@@ -319,7 +324,7 @@ if __name__ == "__main__":
             # and exclude areas where: -max_depth > grid cell depth
             func_depth = functools.partial(np.greater, -config["max_depth"])
             excluder.add_raster(
-                paths.gebco, codes=func_depth, crs="EPSG:4326", nodata=-1000
+                paths.gebco, codes=func_depth, crs=GEBCO_CRS, nodata=-1000
             )
 
         if "min_shore_distance" in config:
@@ -340,7 +345,7 @@ if __name__ == "__main__":
         else:
             availability = cutout.availabilitymatrix(regions, excluder, **kwargs)
 
-        area = cutout.grid.to_crs(3035).area / 1e6
+        area = cutout.grid.to_crs(metric_crs).area / 1e6
         area = xr.DataArray(
             area.values.reshape(cutout.shape), [cutout.coords["y"], cutout.coords["x"]]
         )
