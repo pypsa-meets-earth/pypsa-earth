77605e14dbccabcd114d733299d992d653a64ccd
ekatef <e.v.kasilova@gmail.com>
2022-06-09 22:19:05 +0300

    Comment-out converters (debugging)

--- scripts/build_osm_network.py
+++ scripts/build_osm_network.py
@@ -658,9 +658,9 @@ def merge_stations_lines_by_station_id_and_voltage(
     # append transformer lines
     lines = pd.concat([lines, transformers], ignore_index=True)
 
-    converters = get_converters(buses, lines)
-    if len(links) > 0:
-        links = pd.concat([links, converters], ignore_index=True)
+    # converters = get_converters(buses, lines)
+    # if len(links) > 0:
+    #     links = pd.concat([links, converters], ignore_index=True)
 
     # reset index
     lines.reset_index(drop=True, inplace=True)
