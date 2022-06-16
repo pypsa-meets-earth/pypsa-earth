5c95c9ffbd80f5da8828d4af94dbdf7f7f80ae03
ekatef <e.v.kasilova@gmail.com>
2022-06-16 10:52:08 +0300

    Add simplified converters to the links dataframe

--- scripts/build_osm_network.py
+++ scripts/build_osm_network.py
@@ -658,9 +658,9 @@ def merge_stations_lines_by_station_id_and_voltage(
     # append transformer lines
     lines = pd.concat([lines, transformers], ignore_index=True)
 
-    # converters = get_converters(buses, lines)
-    # if len(links) > 0:
-    #     links = pd.concat([links, converters], ignore_index=True)
+    if len(links) > 0:
+        converters = get_converters(buses, links)
+        links = pd.concat([links, converters], ignore_index=True)
 
     # reset index
     lines.reset_index(drop=True, inplace=True)
