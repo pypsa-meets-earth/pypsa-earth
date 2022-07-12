6c01d291c7871586d86913590752130b6bc10b79
ekatef <e.v.kasilova@gmail.com>
2022-07-12 21:13:54 +0300

    Restore initial settings of the stubs removal

--- scripts/simplify_network.py
+++ scripts/simplify_network.py
@@ -416,9 +416,7 @@ def busmap_by_stubs(network, matching_attrs=None):
 def remove_stubs(n):
     logger.info("Removing stubs")
 
-    # Setting matching_attrs="carrier" is needed to keep DC buses
-    # in case HVDC lines are modeled as the Line component
-    busmap = busmap_by_stubs(n, matching_attrs="carrier")  # ['country'])
+    busmap = busmap_by_stubs(n)
 
     connection_costs_to_bus = _compute_connection_costs_to_bus(n, busmap)
 
