233fbb6f52044364d9351b1ba8c6f607385cd526
Ekaterina-Vo <memmam97@gmail.com>
2022-08-15 18:39:02 +0300

    fix output electricity column

--- scripts/clean_osm_data.py
+++ scripts/clean_osm_data.py
@@ -561,9 +561,18 @@ def prepare_generators_df(df_all_generators):
     """
     # reset index
     df_all_generators = df_all_generators.reset_index(drop=True)
-
-    # rename columns
+        
+    # rename columns    
+    
+    check_fields_for_generators = ['tags.generator:output:electricity']     
+    for field_to_add in check_fields_for_generators:
+        #print(df_all_generators)
+        if field_to_add not in df_all_generators.columns.tolist():
+           df_all_generators[field_to_add]=None
+           print(df_all_generators)
+    
     df_all_generators = df_all_generators.rename(
+        # TODO fix column name for KG
         columns={
             "tags.generator:output:electricity": "power_output_MW",
             "tags.name": "name",
@@ -757,6 +766,7 @@ def clean_data(
 
     # prepare the generator dataset
     df_all_generators = prepare_generators_df(df_all_generators)
+    
 
     # set the country name by the shape
     df_all_generators = set_countryname_by_shape(
