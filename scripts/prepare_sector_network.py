c9b1353afb2fe2420e5553c3ada9397ade401da9
Hazem <hazem.abdel-khalek@ieg.fraunhofer.de>
2022-09-05 17:54:00 +0200

    fix CI

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -2030,13 +2030,13 @@ if __name__ == "__main__":
     # TODO follow the same structure as land transport and heat
 
     # Load industry demand data
-    industrial_demand = pd.read_csv(snakemake.input.industrial_demand)  # * 1e6
+    industrial_demand = pd.read_csv(snakemake.input.industrial_demand, index_col=0)  # * 1e6
 
-    industrial_demand["TWh/a (MtCO2/a)"] = industrial_demand["TWh/a (MtCO2/a)"].apply(
-        lambda cocode: two_2_three_digits_country(cocode[:2]) + cocode[2:]
-    )
+    # industrial_demand["TWh/a (MtCO2/a)"] = industrial_demand["TWh/a (MtCO2/a)"].apply(
+    #     lambda cocode: two_2_three_digits_country(cocode[:2]) + cocode[2:]
+    # )
 
-    industrial_demand.set_index("TWh/a (MtCO2/a)", inplace=True)
+    # industrial_demand.set_index("TWh/a (MtCO2/a)", inplace=True)
     ##########################################################################
     ############## Functions adding different carrires and sectors ###########
     ##########################################################################
