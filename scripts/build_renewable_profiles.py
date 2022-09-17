835e8e2d6568ec250d5e640dc2d686c8d254e48a
ekatef <e.v.kasilova@gmail.com>
2022-09-17 09:34:09 +0300

    Improve warning

--- scripts/build_renewable_profiles.py
+++ scripts/build_renewable_profiles.py
@@ -422,7 +422,7 @@ if __name__ == "__main__":
             if not cutout_in_natura:
                 # TODO Improve notification on natura.tiff coordinates
                 logger.warning(
-                    f"A provided 'natura.tiff' does not contain the selected cutout. The coordinates are in the following range: cutout:left={cutout.bounds[0]:2.2f}, bottom={cutout.bounds[1]:2.2f},right={cutout.bounds[2]:2.2f}, top={cutout.bounds[3]:2.2f}; 'natura.tiff':left={natura_geom.bounds[0]:2.2f}, bottom={natura_geom.bounds[1]:2.2f},right={natura_geom.bounds[2]:2.2f}, top={natura_geom.bounds[3]:2.2f}"
+                    f"A provided 'natura.tiff' does not contain the selected cutout. The coordinates are in the following range:\n\r cutout left={cutout.bounds[0]:2.1f}, bottom={cutout.bounds[1]:2.1f}, right={cutout.bounds[2]:2.1f}, top={cutout.bounds[3]:2.1f};\n\r 'natura.tiff' left={natura_geom.bounds[0]:2.1f}, bottom={natura_geom.bounds[1]:2.1f},right={natura_geom.bounds[2]:2.1f}, top={natura_geom.bounds[3]:2.2f}"
                 )
             excluder.add_raster(paths.natura, nodata=0, allow_no_overlap=True)
 
