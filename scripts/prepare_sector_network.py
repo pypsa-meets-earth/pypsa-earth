fix bugs with scaling in prepare_sector_networks and snakemake networks loadsing

scripts/prepare_sector_network.py
@@ -660,7 +660,7 @@ def add_aviation(n, cost):
     all_aviation = ["total international aviation", "total domestic aviation"]
 
     aviation_demand = (
-        energy_totals.loc[countries, all_aviation].sum(axis=1).sum() * 1e6 / 8760
+        energy_totals.loc[countries, all_aviation].sum(axis=1).sum() #* 1e6 / 8760
     )
 
     airports = pd.read_csv(snakemake.input.airports)
@@ -690,6 +690,7 @@ def add_aviation(n, cost):
     airports["p_set"] = airports["fraction"].apply(
         lambda frac: frac
         * aviation_demand
+        *1e6
         / 8760  # TODO change the way pset is sampled here
         # the current way leads to inaccuracies in the last timestep in case
         # the timestep if 8760 is not divisble by it),
@@ -859,7 +860,7 @@ def add_shipping(n, costs):
     all_navigation = ["total international navigation", "total domestic navigation"]
 
     navigation_demand = (
-        energy_totals.loc[countries, all_navigation].sum(axis=1).sum() * 1e6 / 8760
+        energy_totals.loc[countries, all_navigation].sum(axis=1).sum() #* 1e6 / 8760
     )
 
     efficiency = (
@@ -2004,12 +2005,12 @@ if __name__ == "__main__":
         snakemake = mock_snakemake(
             "prepare_sector_network",
             simpl="",
-            clusters="4",
-            ll="c1",
+            clusters="22",
+            ll="c1.0",
             opts="Co2L",
             planning_horizons="2030",
-            sopts="144H",
-            discountrate="0.071",
+            sopts="730H",
+            discountrate="0.069",
             demand="NZ",
         )
 