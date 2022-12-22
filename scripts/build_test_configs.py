fba027a4ea21596984d4cd760eceebcec12eaf42
davide-f <fioritidavidesubs@gmail.com>
2022-12-22 22:51:07 +0100

    Revise test config style

--- scripts/build_test_configs.py
+++ scripts/build_test_configs.py
@@ -26,6 +26,27 @@ def update(d, u):
     return d
 
 
+def create_test_config(base_path, changes_path, output_path):
+    # Input paths
+    fp_baseconfig = Path(Path.cwd(), base_path)
+    fp_update_file = Path(Path.cwd(), changes_path)
+
+    # Load yaml files
+    yaml = YAML()
+    with open(fp_baseconfig) as fp:
+        base_config = yaml.load(fp)
+
+    # Load update yaml
+    with open(fp_update_file) as fp:
+        update_config = yaml.load(fp)
+    # create updated yaml
+    test_config = update(copy.deepcopy(base_config), update_config)
+    # Output path
+    fp = Path(Path.cwd(), output_path)
+    # Save file
+    yaml.dump(test_config, fp)
+
+
 if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
@@ -34,27 +55,12 @@ if __name__ == "__main__":
         snakemake = mock_snakemake("build_test_configs")
 
     # Input paths
-    fp_baseconfig = Path(Path.cwd(), snakemake.input.base_config)
-    fp_update_file_list = [
-        Path(Path.cwd(), i) for i in snakemake.input.update_file_list
-    ]
+    fp_baseconfig = snakemake.input.base_config
+    fp_update_file_list = snakemake.input.update_file_list
+    fp_output_file_list = snakemake.output.tmp_test_configs
 
-    # Load yaml files
-    yaml = YAML()
-    with open(fp_baseconfig) as fp:
-        base_config = yaml.load(fp)
-
-    for c in fp_update_file_list:
-        # Load update yaml
-        with open(c) as fp:
-            update_config = yaml.load(fp)
-        # create updated yaml
-        test_config = update(copy.deepcopy(base_config), update_config)
-        # Output path
-        list_no = fp_update_file_list.index(c)
-        fp = Path(Path.cwd(), snakemake.output[list_no])
-        # Save file
-        yaml.dump(test_config, fp)
+    for (finput, foutput) in zip(fp_update_file_list, fp_output_file_list):
+        create_test_config(fp_baseconfig, finput, foutput)
 
     # Manual output in terminal
     # import sys
