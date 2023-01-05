e9939c1e24eb30b74489e989400c64ace9f518ed
ekatef <e.v.kasilova@gmail.com>
2023-01-06 00:29:33 +0300

    Remove TODOs

--- scripts/download_osm_data.py
+++ scripts/download_osm_data.py
@@ -46,9 +46,7 @@ from tqdm import tqdm
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# TODO logger name should be the same across all the scripts
 logger = logging.getLogger(__name__)
-# TODO check if duplication with basicConfig() is really needed
 
 
 OSM_CRS = "EPSG:4326"
