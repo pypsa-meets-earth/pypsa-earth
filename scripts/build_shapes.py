7cb281236a1a330c1751dcaaf8eec412a791ba1d
ekatef <e.v.kasilova@gmail.com>
2023-01-21 01:12:37 +0300

    Fix defaults

--- scripts/build_shapes.py
+++ scripts/build_shapes.py
@@ -179,7 +179,7 @@ def get_GADM_layer(
             layer=layer_id,
             cc=country_code,
             contended_flag=contended_flag,
-            output_nonstd_to_csv=True,
+            output_nonstd_to_csv=False,
         )
 
         # create a subindex column that is useful
