9233a3b6b1f01538e379f6f13e856dd4043913a1
energyls <leon1.schumm@oth-regensburg.de>
2023-02-16 12:37:14 +0100

    fix: linting issue

--- sample_pypsa_eur/prepare_sector_network_eur_sec.py
+++ sample_pypsa_eur/prepare_sector_network_eur_sec.py
@@ -98,38 +98,38 @@ def get(item, investment_year=None):
     else:
         return item
 
+# TODO function redefinition
+# def create_network_topology(n, prefix, connector=" -> "):
+#     """
+#     Create a network topology like the power transmission network.
 
-def create_network_topology(n, prefix, connector=" -> "):
-    """
-    Create a network topology like the power transmission network.
+#     Parameters
+#     ----------
+#     n : pypsa.Network
+#     prefix : str
+#     connector : str
 
-    Parameters
-    ----------
-    n : pypsa.Network
-    prefix : str
-    connector : str
+#     Returns
+#     -------
+#     pd.DataFrame with columns bus0, bus1 and length
+#     """
 
-    Returns
-    -------
-    pd.DataFrame with columns bus0, bus1 and length
-    """
+#     ln_attrs = ["bus0", "bus1", "length"]
+#     lk_attrs = ["bus0", "bus1", "length", "underwater_fraction"]
 
-    ln_attrs = ["bus0", "bus1", "length"]
-    lk_attrs = ["bus0", "bus1", "length", "underwater_fraction"]
+#     candidates = pd.concat(
+#         [n.lines[ln_attrs], n.links.loc[n.links.carrier == "DC", lk_attrs]]
+#     ).fillna(0)
 
-    candidates = pd.concat(
-        [n.lines[ln_attrs], n.links.loc[n.links.carrier == "DC", lk_attrs]]
-    ).fillna(0)
+#     positive_order = candidates.bus0 < candidates.bus1
+#     candidates_p = candidates[positive_order]
+#     swap_buses = {"bus0": "bus1", "bus1": "bus0"}
+#     candidates_n = candidates[~positive_order].rename(columns=swap_buses)
+#     candidates = pd.concat([candidates_p, candidates_n])
 
-    positive_order = candidates.bus0 < candidates.bus1
-    candidates_p = candidates[positive_order]
-    swap_buses = {"bus0": "bus1", "bus1": "bus0"}
-    candidates_n = candidates[~positive_order].rename(columns=swap_buses)
-    candidates = pd.concat([candidates_p, candidates_n])
-
-    topo = candidates.groupby(["bus0", "bus1"], as_index=False).mean()
-    topo.index = topo.apply(lambda c: prefix + c.bus0 + connector + c.bus1, axis=1)
-    return topo
+#     topo = candidates.groupby(["bus0", "bus1"], as_index=False).mean()
+#     topo.index = topo.apply(lambda c: prefix + c.bus0 + connector + c.bus1, axis=1)
+#     return topo
 
 
 def co2_emissions_year(countries, opts, year):
@@ -913,47 +913,47 @@ def add_generation(n, costs):
             lifetime=costs.at[generator, "lifetime"],
         )
 
+# TODO locations was missing
+# def add_wave(n, wave_cost_factor):
 
-def add_wave(n, wave_cost_factor):
+#     # TODO: handle in Snakefile
+#     wave_fn = "data/WindWaveWEC_GLTB.xlsx"
 
-    # TODO: handle in Snakefile
-    wave_fn = "data/WindWaveWEC_GLTB.xlsx"
+#     # in kW
+#     capacity = pd.Series({"Attenuator": 750, "F2HB": 1000, "MultiPA": 600})
 
-    # in kW
-    capacity = pd.Series({"Attenuator": 750, "F2HB": 1000, "MultiPA": 600})
+#     # in EUR/MW
+#     annuity_factor = annuity(25, 0.07) + 0.03
+#     costs = (
+#         1e6
+#         * wave_cost_factor
+#         * annuity_factor
+#         * pd.Series({"Attenuator": 2.5, "F2HB": 2, "MultiPA": 1.5})
+#     )
 
-    # in EUR/MW
-    annuity_factor = annuity(25, 0.07) + 0.03
-    costs = (
-        1e6
-        * wave_cost_factor
-        * annuity_factor
-        * pd.Series({"Attenuator": 2.5, "F2HB": 2, "MultiPA": 1.5})
-    )
+#     sheets = pd.read_excel(
+#         wave_fn,
+#         sheet_name=["FirthForth", "Hebrides"],
+#         usecols=["Attenuator", "F2HB", "MultiPA"],
+#         index_col=0,
+#         skiprows=[0],
+#         parse_dates=True,
+#     )
 
-    sheets = pd.read_excel(
-        wave_fn,
-        sheet_name=["FirthForth", "Hebrides"],
-        usecols=["Attenuator", "F2HB", "MultiPA"],
-        index_col=0,
-        skiprows=[0],
-        parse_dates=True,
-    )
+#     wave = pd.concat(
+#         [sheets[l].divide(capacity, axis=1) for l in locations], keys=locations, axis=1
+#     )
 
-    wave = pd.concat(
-        [sheets[l].divide(capacity, axis=1) for l in locations], keys=locations, axis=1
-    )
-
-    for wave_type in costs.index:
-        n.add(
-            "Generator",
-            "Hebrides " + wave_type,
-            bus="GB4 0",  # TODO this location is hardcoded
-            p_nom_extendable=True,
-            carrier="wave",
-            capital_cost=costs[wave_type],
-            p_max_pu=wave["Hebrides", wave_type],
-        )
+#     for wave_type in costs.index:
+#         n.add(
+#             "Generator",
+#             "Hebrides " + wave_type,
+#             bus="GB4 0",  # TODO this location is hardcoded
+#             p_nom_extendable=True,
+#             carrier="wave",
+#             capital_cost=costs[wave_type],
+#             p_max_pu=wave["Hebrides", wave_type],
+#         )
 
 
 def insert_electricity_distribution_grid(n, costs):
