81e02b1420c803a7ce27ec96fe0b06d41511cf0e
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2023-05-21 00:45:07 +0600

    Custom powerplant functionalities

--- scripts/build_powerplants.py
+++ scripts/build_powerplants.py
@@ -247,9 +247,10 @@ def add_custom_powerplants(ppl, inputs, config):
     # if isinstance(custom_ppl_query, str):
     #     add_ppls.query(custom_ppl_query, inplace=True)
 
-    return pd.concat(
-        [ppl, add_ppls], sort=False, ignore_index=True, verify_integrity=True
-    )
+    if custom_ppl_query == "merge":
+        return pd.concat([ppl, add_ppls], sort=False, ignore_index=True, verify_integrity=True)
+    elif custom_ppl_query == "replace":
+        return add_ppls
 
 
 def replace_natural_gas_technology(df):
