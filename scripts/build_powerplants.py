150d547014343a0b7c63165666e6a9637b430567
yerbol-akhmetov <yerbol.akhmetov.93@gmail.com>
2023-05-21 18:56:26 +0600

    Remove redundant lines

--- scripts/build_powerplants.py
+++ scripts/build_powerplants.py
@@ -244,8 +244,6 @@ def add_custom_powerplants(ppl, inputs, config):
     add_ppls = read_csv_nafix(
         inputs.custom_powerplants, index_col=0, dtype={"bus": "str"}
     )
-    # if isinstance(custom_ppl_query, str):
-    #     add_ppls.query(custom_ppl_query, inplace=True)
 
     if custom_ppl_query == "merge":
         return pd.concat(
