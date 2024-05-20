1634082c97543e9070ea7c5ad9981e8264c77134
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-05-20 13:35:49 +0200

    add unit test

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -6,7 +6,12 @@
 # -*- coding: utf-8 -*-
 
 import pathlib
-from scripts._helpers import sets_path_to_root, two_2_three_digits_country, three_2_two_digits_country
+from scripts._helpers import (
+    country_name_2_two_digits,
+    sets_path_to_root,
+    two_2_three_digits_country,
+    two_digits_2_name_country,
+    three_2_two_digits_country,)
 
 path_cwd = pathlib.Path.cwd()
 
@@ -51,3 +56,27 @@ def test_three_2_two_digits_country():
     assert "FM" == three_2_two_digits_country("FSM")
 
 
+def test_two_digits_2_name_country():
+    """
+    Verify the conversion from two-digit country code to country name
+    """
+    # Micronesia (Federated States of)
+    assert "Micronesia, Fed. Sts." == two_digits_2_name_country("FM")
+    assert "Fed. Sts. Micronesia" == two_digits_2_name_country("FM", nocomma=True)
+    assert "Sts. Micronesia" == two_digits_2_name_country("FM", nocomma=True, remove_start_words=["Fed. "])
+
+
+def test_country_name_2_two_digits():
+    """
+    Verify the conversion from country name to two-digit country code
+    """
+    # Afghanistan
+    assert "AF" == country_name_2_two_digits("Afghanistan")
+    # American Samoa
+    assert "AS" == country_name_2_two_digits("American Samoa")
+    # Aruba
+    assert "AW" == country_name_2_two_digits("Aruba")
+    # Germany
+    assert "DE" == country_name_2_two_digits("Germany")
+    # Micronesia (Federated States of)
+    assert "FM" == country_name_2_two_digits("Micronesia")
