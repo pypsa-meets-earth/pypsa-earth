5c19e948bc1bec7442129bb1e02e7771d5df8bd0
davide-f <fioritidavidesubs@gmail.com>
2024-07-10 22:35:53 +0200

    Set location to Earth

--- scripts/add_export.py
+++ scripts/add_export.py
@@ -77,7 +77,7 @@ def add_export(n, hydrogen_buses_ports, export_profile):
         "Bus",
         "H2 export bus",
         carrier="H2",
-        location="World",  # TODO: revise
+        location="Earth",
         x=x_export,
         y=y_export,
     )
