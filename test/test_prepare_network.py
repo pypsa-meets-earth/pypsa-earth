2ee715d9b33b40d40c971f7f5c5c8d5255993345
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-08-10 02:07:34 +0200

    add unit test for add_co2limit

--- test/test_prepare_network.py
+++ test/test_prepare_network.py
@@ -7,18 +7,18 @@
 
 import sys
 
-import numpy as np
-import pandas as pd
+import pypsa
 
 sys.path.append("./scripts")
 
-from prepare_network import download_emission_data, emission_extractor
+from prepare_network import add_co2limit, download_emission_data, emission_extractor
 
 emissions_file_url = "https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/EDGAR/datasets/v60_GHG/CO2_excl_short-cycle_org_C/v60_GHG_CO2_excl_short-cycle_org_C_1970_2018.zip"
 emissions_file_name = "v60_CO2_excl_short-cycle_org_C_1970_2018.xls"
 emissions_sheet_name = "v6.0_EM_CO2_fossil_IPCC1996"
 automatic_emission_base_year = 1990
 country_names = ["DE", "IT", "NG"]
+co2limit = 7.75e7
 
 
 def test_download_emission_data():
@@ -39,3 +39,15 @@ def test_emission_extractor():
         381475.887377666,
         123981.6458729,
     ]
+
+
+def test_add_co2limit():
+    test_network_de = pypsa.examples.scigrid_de(from_master=True)
+    number_years = test_network_de.snapshot_weightings.objective.sum() / 8760.0
+    add_co2limit(test_network_de, co2limit, number_years)
+    assert (
+        test_network_de.global_constraints.carrier_attribute.values[0]
+        == "co2_emissions"
+    )
+    assert test_network_de.global_constraints.sense.values[0] == "<="
+    assert test_network_de.global_constraints.constant.values[0] == 212328.76712328766
