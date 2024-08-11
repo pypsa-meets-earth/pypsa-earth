71b8c787dd7410e77c19643d3f4741e24ac11bfa
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-08-11 04:45:54 +0200

    add test for enforce_autarky

--- test/test_prepare_network.py
+++ test/test_prepare_network.py
@@ -13,9 +13,12 @@ sys.path.append("./scripts")
 
 from prepare_network import (
     add_co2limit,
+    add_emission_prices,
     add_gaslimit,
     download_emission_data,
     emission_extractor,
+    enforce_autarky,
+    set_line_s_max_pu,
 )
 
 emissions_file_url = "https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/EDGAR/datasets/v60_GHG/CO2_excl_short-cycle_org_C/v60_GHG_CO2_excl_short-cycle_org_C_1970_2018.zip"
@@ -27,8 +30,8 @@ co2limit = 7.75e7
 
 
 def test_download_emission_data():
-    filename = download_emission_data(emissions_file_url, emissions_file_name)
-    assert filename == emissions_file_name
+    file_name = download_emission_data(emissions_file_url, emissions_file_name)
+    assert file_name == emissions_file_name
 
 
 def test_emission_extractor():
@@ -71,3 +74,92 @@ def test_add_gaslimit():
         test_network_de.global_constraints.constant.values[0]
         == number_years * number_years
     )
+
+
+def test_set_line_s_max_pu():
+    test_network_de = pypsa.examples.scigrid_de(from_master=True)
+    s_max_pu_new_value = 3.0
+    set_line_s_max_pu(test_network_de, s_max_pu_new_value)
+    assert test_network_de.lines["s_max_pu"].unique()[0] == s_max_pu_new_value
+
+
+def test_enforce_autarky():
+
+    # test with only_crossborder=False
+    test_network_no_cross_border = pypsa.examples.ac_dc_meshed(from_master=True)
+
+    bus_country_list = ["UK", "UK", "UK", "UK", "DE", "DE", "DE", "NO", "NO"]
+    test_network_no_cross_border.buses["country"] = bus_country_list
+    test_network_no_cross_border.links["carrier"] = "DC"
+
+    enforce_autarky(test_network_no_cross_border, only_crossborder=False)
+
+    output_component_dict_no_cross_border = {}
+    for c in test_network_no_cross_border.iterate_components(
+        list(test_network_no_cross_border.components.keys())[2:]
+    ):
+        output_component_dict_no_cross_border[c.name] = len(c.df)
+
+    reference_component_dict_no_cross_border = {
+        "Bus": 9,
+        "Carrier": 3,
+        "GlobalConstraint": 1,
+        "LineType": 34,
+        "TransformerType": 14,
+        "Load": 6,
+        "Generator": 6,
+    }
+    assert (
+        output_component_dict_no_cross_border
+        == reference_component_dict_no_cross_border
+    )
+
+    # test with only_crossborder=True
+    test_network_with_cross_border = pypsa.examples.ac_dc_meshed(from_master=True)
+    bus_country_list = ["UK", "UK", "UK", "UK", "DE", "DE", "DE", "NO", "NO"]
+    test_network_with_cross_border.buses["country"] = bus_country_list
+    test_network_with_cross_border.links["carrier"] = "DC"
+
+    enforce_autarky(test_network_with_cross_border, only_crossborder=True)
+
+    output_component_dict_with_cross_border = {}
+    for c in test_network_with_cross_border.iterate_components(
+        list(test_network_with_cross_border.components.keys())[2:]
+    ):
+        output_component_dict_with_cross_border[c.name] = len(c.df)
+
+    reference_component_dict_with_cross_border = {
+        "Bus": 9,
+        "Carrier": 3,
+        "GlobalConstraint": 1,
+        "Line": 4,
+        "LineType": 34,
+        "TransformerType": 14,
+        "Link": 3,
+        "Load": 6,
+        "Generator": 6,
+    }
+    assert (
+        output_component_dict_with_cross_border
+        == reference_component_dict_with_cross_border
+    )
+
+
+# I do not understand this method!
+# def test_add_emission_prices():
+#     test_network_de = pypsa.examples.scigrid_de(from_master=True)
+#     test_network_de.add("Carrier", "OCGT_emissions", co2_emissions=2.0)
+#     test_network_de.add("Carrier", "coal_emissions", co2_emissions=2.0)
+#     test_network_de.add("Generator", "OCGT_emissions")
+#     test_network_de.add("Generator", "coal_emissions")
+#     ep = (
+#         pd.Series({"co2": 1.0}).rename(lambda x: x + "_emissions")
+#         * test_network_de.carriers.filter(like="_emissions")
+#     ).sum(axis=1)
+#     print("first term", test_network_de.generators.carrier.map(ep))
+#     print("second term", test_network_de.generators.efficiency)
+#     #print("before", test_network_de.generators["marginal_cost"])
+#     #print("=====")
+#     add_emission_prices(test_network_de, emission_prices={"co2": 1.0}, exclude_co2=False)
+#     #print("after", test_network_de.generators["marginal_cost"])
+#     assert False
