db728f65c94f52e83caf0d53e767741c17f2267c
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-08-13 02:04:47 +0200

    add unit tests for average_every_nhours and add_emission_prices

--- test/test_prepare_network.py
+++ test/test_prepare_network.py
@@ -8,6 +8,7 @@
 import sys
 
 import pypsa
+from pandas import Timestamp
 
 sys.path.append("./scripts")
 
@@ -15,6 +16,7 @@ from prepare_network import (
     add_co2limit,
     add_emission_prices,
     add_gaslimit,
+    average_every_nhours,
     download_emission_data,
     emission_extractor,
     enforce_autarky,
@@ -31,11 +33,17 @@ co2limit = 7.75e7
 
 
 def test_download_emission_data():
+    """
+    Verify what returned by download_emission_data.
+    """
     file_name = download_emission_data(emissions_file_url, emissions_file_name)
     assert file_name == emissions_file_name
 
 
 def test_emission_extractor():
+    """
+    Verify what returned by emission_extractor.
+    """
     output_series = emission_extractor(
         emissions_file_name,
         emissions_sheet_name,
@@ -51,6 +59,9 @@ def test_emission_extractor():
 
 
 def test_add_co2limit():
+    """
+    Verify what returned by add_co2limit.
+    """
     test_network_de = pypsa.examples.scigrid_de(from_master=True)
     number_years = test_network_de.snapshot_weightings.objective.sum() / 8760.0
     add_co2limit(test_network_de, co2limit, number_years)
@@ -65,6 +76,9 @@ def test_add_co2limit():
 
 
 def test_add_gaslimit():
+    """
+    Verify what returned by add_gaslimit.
+    """
     test_network_de = pypsa.examples.scigrid_de(from_master=True)
     test_network_de.add("Carrier", "OCGT")
     number_years = test_network_de.snapshot_weightings.objective.sum() / 8760.0
@@ -78,35 +92,68 @@ def test_add_gaslimit():
 
 
 def test_add_emission_prices():
+    """
+    Verify what returned by add_emission_prices.
+    """
     test_network_ac_dc_meshed = pypsa.examples.ac_dc_meshed(from_master=True)
-    print("before", test_network_ac_dc_meshed.generators["marginal_cost"])
-    print("=====")
     add_emission_prices(
         test_network_ac_dc_meshed, emission_prices={"co2": 1.0}, exclude_co2=False
     )
-    print("after", test_network_ac_dc_meshed.generators["marginal_cost"])
-    assert False
+    assert test_network_ac_dc_meshed.generators["marginal_cost"].index.tolist() == [
+        "Manchester Wind",
+        "Manchester Gas",
+        "Norway Wind",
+        "Norway Gas",
+        "Frankfurt Wind",
+        "Frankfurt Gas",
+    ]
+    assert test_network_ac_dc_meshed.generators["marginal_cost"].values.tolist() == [
+        0.11,
+        5.218030132047726,
+        0.09,
+        6.565421697244602,
+        0.1,
+        4.768788024122113,
+    ]
 
 
 def test_set_line_s_max_pu():
+    """
+    Verify what returned by set_line_s_max_pu.
+    """
     test_network_de = pypsa.examples.scigrid_de(from_master=True)
     s_max_pu_new_value = 3.0
     set_line_s_max_pu(test_network_de, s_max_pu_new_value)
     assert test_network_de.lines["s_max_pu"].unique()[0] == s_max_pu_new_value
 
 
-def test_set_line_nom_max():
-    test_network = pypsa.examples.ac_dc_meshed(from_master=True)
-    s_nom_max_value = 5.0
-    p_nom_max_value = 10.0
-    set_line_nom_max(
-        test_network, s_nom_max_set=s_nom_max_value, p_nom_max_set=p_nom_max_value
-    )
-    assert test_network.lines.s_nom_max.unique()[0] == s_nom_max_value
-    assert test_network.links.p_nom_max.unique()[0] == p_nom_max_value
+def test_average_every_nhours():
+    """
+    Verify what returned by average_every_nhours.
+    """
+    test_network_de = pypsa.examples.scigrid_de(from_master=True)
+
+    # The input network is already sampled in 1H snapshots.
+    # Hence, average_every_nhours should not change anything
+    new_network_1h = average_every_nhours(test_network_de, "1H")
+    assert test_network_de.snapshots.tolist() == new_network_1h.snapshots.tolist()
+
+    # Re-sample to 4H intervals
+    new_network_4h = average_every_nhours(test_network_de, "4H")
+    assert new_network_4h.snapshots.tolist() == [
+        Timestamp("2011-01-01 00:00:00"),
+        Timestamp("2011-01-01 04:00:00"),
+        Timestamp("2011-01-01 08:00:00"),
+        Timestamp("2011-01-01 12:00:00"),
+        Timestamp("2011-01-01 16:00:00"),
+        Timestamp("2011-01-01 20:00:00"),
+    ]
 
 
 def test_enforce_autarky():
+    """
+    Verify what returned by enforce_autarky.
+    """
 
     # test with only_crossborder=False
     # --> it removes all lines and all DC links
@@ -168,3 +215,17 @@ def test_enforce_autarky():
         output_component_dict_with_cross_border
         == reference_component_dict_with_cross_border
     )
+
+
+def test_set_line_nom_max():
+    """
+    Verify what returned by set_line_nom_max.
+    """
+    test_network = pypsa.examples.ac_dc_meshed(from_master=True)
+    s_nom_max_value = 5.0
+    p_nom_max_value = 10.0
+    set_line_nom_max(
+        test_network, s_nom_max_set=s_nom_max_value, p_nom_max_set=p_nom_max_value
+    )
+    assert test_network.lines.s_nom_max.unique()[0] == s_nom_max_value
+    assert test_network.links.p_nom_max.unique()[0] == p_nom_max_value
