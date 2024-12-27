95c51d8e79958a61779716ceefc82460495d3363
ekatef <e.v.kasilova@gmail.com>
2024-12-27 18:58:49 +0100

    Place the code to scripts folder

--- scripts/conservancy_checks.py
+++ scripts/conservancy_checks.py
@@ -11,6 +11,7 @@ import pathlib
 
 import pandas as pd
 import pypsa
+from _helpers import configure_logging, create_logger, mock_snakemake
 
 
 def extract_gen_sum(n, variable_id):
@@ -21,12 +22,7 @@ def extract_gen_sum(n, variable_id):
     return (vals, [variable_id] * len(vals), crrs)
 
 
-# for fl in list(pathlib.Path(data_dir).glob("*.nc")):
-    fl_id = "elec"
-    n = pypsa.Network(fl)
-
-    network_carriers = list(n.generators.carrier.unique())
-
+def collect_statistics(n, network_id):
     values = []
     variables = []
     carriers = []
@@ -41,6 +37,7 @@ def extract_gen_sum(n, variable_id):
         variables.extend(extract_gen_sum(n, var)[1])
         carriers.extend(extract_gen_sum(n, var)[2])
 
+    network_carriers = list(n.generators.carrier.unique())
     for carr in network_carriers:
         p_max_pu_cols = n.generators_t.p_max_pu.columns
         carr_cols = p_max_pu_cols[p_max_pu_cols.str.contains(carr)]
@@ -59,13 +56,32 @@ def extract_gen_sum(n, variable_id):
         variables.extend(["tech_potential"] * len(tech_pot_vals))
         carriers.extend([carr] * len(tech_pot_vals))
 
-    print(values)
-    print(variables)
-    print(carriers)
+    # print(values)
+    # print(variables)
+    # print(carriers)
 
     res_df = pd.DataFrame(
         data={"values": values, "variables": variables, "carriers": carriers}
     )
+    res_df["network_id"] = network_id + ".nc"
 
-    res_df["network_id"] = fl_id + ".nc"
-    res_df.to_csv(fl_id + "_invar_check.csv")
+    return res_df
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("conservancy_checks")
+
+    configure_logging(snakemake)
+
+    data_folder = snakemake.input.network_folder
+
+    for fl in list(pathlib.Path(data_folder).glob("*.nc")):
+        n = pypsa.Network(fl)
+
+        network_data_df = collect_statistics(n, fl.stem)
+
+        network_data_df.to_csv(data_folder + "/" + fl.stem + "_invar_check.csv")
+        # network_data_df.to_csv(data_folder + "/conservation_checks/" + network_id + "_invar_check.csv")
