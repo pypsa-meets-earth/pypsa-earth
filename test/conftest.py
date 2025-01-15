a6b74ddbd6a5fe10566f18c73af74cb527042b36
Emmanuel Bolarinwa <gbotemibolarinwa@gmail.com>
2025-01-15 18:43:57 +0100

    battery constraints

---
+++ test/conftest.py
@@ -0,0 +1,24 @@
+import pypsa
+import pytest
+import pandas as pd
+
+
+@pytest.fixture
+def network():
+    n = pypsa.Network()
+    n.set_snapshots(pd.date_range("2022-01-01", periods=24, freq="H"))
+    n.add("Bus", "bus1")
+    n.add("Link", "battery discharger", bus0="bus1",
+            bus1="bus1", p_nom_extendable=True, efficiency=0.9)
+    n.add("Link", "battery charger", bus0="bus1",
+            bus1="bus1", p_nom_extendable=True)
+    
+    # Add links
+    n.add("Link", "urban central CHP electric", bus0="bus0", bus1="bus1",
+            p_nom_extendable=True, efficiency=0.9, p_nom_ratio=1.0)
+    n.add("Link", "urban central CHP heat", bus0="bus1",
+            bus1="bus0", p_nom_extendable=True, efficiency=0.9)
+
+    n.optimize.create_model()
+
+    return n
---
+++ test/test_solve_network_constraints.py
@@ -0,0 +1,43 @@
+import pytest
+import pypsa
+import sys
+sys.path.append("./scripts")
+import _helpers
+from solve_network import add_battery_constraints, add_chp_constraints
+from test.conftest import network
+
+
+
+def test_add_battery_constraints(network):
+
+    # Apply the function
+    add_battery_constraints(network)
+
+    constraints = network.model.constraints["Link-charger_ratio"]
+    assert constraints is not None
+
+    # Check that the constraints were added
+    assert "Link-charger_ratio" in network.model.constraints
+
+    # Check the constraint expression
+    lhs = network.model.constraints["Link-charger_ratio"][0].lhs
+
+    assert lhs.equals(network.model["Link-p_nom"].loc["battery charger"] -
+                    0.9 * network.model["Link-p_nom"].loc["battery discharger"])
+    
+def test_chp_constraints(network):
+
+    # Apply the function
+    add_chp_constraints(network)
+
+    constraints = network.model.constraints["Link-CHP_ratio"]
+    assert constraints is not None
+
+    # Check that the constraints were added
+    assert "Link-CHP_ratio" in network.model.constraints
+
+    # Check the constraint expression
+    lhs = network.model.constraints["Link-CHP_ratio"][0].lhs
+
+    assert lhs.equals(network.model["Link-p_nom"].loc["urban central CHP heat"] -
+                    0.9 * network.model["Link-p_nom"].loc["urban central CHP electric"])
