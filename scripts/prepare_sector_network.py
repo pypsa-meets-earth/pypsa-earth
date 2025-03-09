e961cbb6a81f6e5c752140a498f5f81adca68487
Davide Fioriti <67809479+davide-f@users.noreply.github.com>
2025-03-09 19:51:24 +0100

    Update prepare_sector_network.py

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -2930,7 +2930,6 @@ if __name__ == "__main__":
     # countries = list(n.buses.country.unique())
     countries = snakemake.config["countries"]
     # Locate all the AC buses
-    # breakpoint()
     nodes = n.buses[
         n.buses.carrier == "AC"
     ].index  # TODO if you take nodes from the index of buses of n it's more than pop_layout
@@ -2940,7 +2939,6 @@ if __name__ == "__main__":
     n.buses.location = n.buses.index
 
     # Set carrier of AC loads
-    # breakpoint()
     n.loads.loc[nodes, "carrier"] = "AC"
 
     Nyears = n.snapshot_weightings.generators.sum() / 8760
