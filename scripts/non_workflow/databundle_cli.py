0e687571325f132ee3c182aa41a32691f671f7b0
Virio Andreyana <114650479+virio-andreyana@users.noreply.github.com>
2025-04-24 10:34:56 +0200

    Update scripts/non_workflow/databundle_cli.py
    Co-authored-by: Ekaterina <e.v.kasilova@gmail.com>

--- scripts/non_workflow/databundle_cli.py
+++ scripts/non_workflow/databundle_cli.py
@@ -156,7 +156,8 @@ if __name__ == "__main__":
 
     """
     )
-
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
     snakemake = mock_snakemake("retrieve_databundle_light")
 
     rootpath = "."
