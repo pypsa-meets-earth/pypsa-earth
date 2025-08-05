ff804036783d96ea80bb81cec98133a5fc74fa00
Eric Nitschke <eric.nitschke@tuhh.de>
2025-08-05 13:05:03 +0200

    Reactivate the option for a custom_busmap
    Minor changes:
    - fix the implementation of a custom_busmap in cluster_network.py
    - reactivate the option for a custom_busmap in Snakefile
    - add an option for a custom_busmap to the config
    - add a basic custom_busmap.csv

--- scripts/cluster_network.py
+++ scripts/cluster_network.py
@@ -709,13 +709,15 @@ if __name__ == "__main__":
             },
         )
 
-        custom_busmap = False  # snakemake.params.custom_busmap custom busmap is depreciated https://github.com/pypsa-meets-earth/pypsa-earth/pull/694
+        custom_busmap = snakemake.params.custom_busmap
         if custom_busmap:
             busmap = pd.read_csv(
-                snakemake.input.custom_busmap, index_col=0, squeeze=True
-            )
+                snakemake.input.custom_busmap, index_col=0
+            ).squeeze()
             busmap.index = busmap.index.astype(str)
             logger.info(f"Imported custom busmap from {snakemake.input.custom_busmap}")
+            custom_busmap = busmap
+
         cluster_config = snakemake.config.get("cluster_options", {}).get(
             "cluster_network", {}
         )
