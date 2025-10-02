88911cf007ddad7949d3d82187fe68d26c8a8de4
Virio Andreyana <114650479+virio-andreyana@users.noreply.github.com>
2025-10-02 21:56:14 +0200

    Bugfix: Fix shipping and aviation implementation in multi-country models (#1582)
    * Bugfix: Fix shipping and aviation distribution in multi-country calculations
    
    Co-authored-by: filleafkhr <charlottelesile23@gmail.com>
    
    * add documentation
    
    Co-authored-by: filleafkhr <charlottelesile23@gmail.com>
    
    * fix ports capacity in shipping oil demand
    
    ---------
    
    Co-authored-by: filleafkhr <charlottelesile23@gmail.com>

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -1231,9 +1231,7 @@ def add_aviation(n, cost, energy_totals, airports_fn):
 
     all_aviation = ["total international aviation", "total domestic aviation"]
 
-    aviation_demand = (
-        energy_totals.loc[countries, all_aviation].sum(axis=1).sum()  # * 1e6 / 8760
-    )
+    aviation_demand = energy_totals.loc[countries, all_aviation].sum(axis=1)
 
     airports = pd.read_csv(airports_fn, keep_default_na=False)
     airports = airports[airports.country.isin(countries)]
@@ -1252,8 +1250,8 @@ def add_aviation(n, cost, energy_totals, airports_fn):
     ind = pd.DataFrame(n.buses.index[n.buses.carrier == "AC"])
 
     ind = ind.set_index(n.buses.index[n.buses.carrier == "AC"])
-    airports["p_set"] = airports["fraction"].apply(
-        lambda frac: frac * aviation_demand * 1e6 / 8760
+    airports["p_set"] = (
+        airports["fraction"] * airports["country"].map(aviation_demand) * 1e6 / 8760
     )
 
     airports = pd.concat([airports, ind])
@@ -1391,9 +1389,7 @@ def add_shipping(n, costs, energy_totals, ports_fn):
 
     all_navigation = ["total international navigation", "total domestic navigation"]
 
-    navigation_demand = (
-        energy_totals.loc[countries, all_navigation].sum(axis=1).sum()  # * 1e6 / 8760
-    )
+    navigation_demand = energy_totals.loc[countries, all_navigation].sum(axis=1)
 
     efficiency = (
         options["shipping_average_efficiency"] / costs.at["fuel cell", "efficiency"]
@@ -1415,10 +1411,10 @@ def add_shipping(n, costs, energy_totals, ports_fn):
     ind = pd.DataFrame(n.buses.index[n.buses.carrier == "AC"])
     ind = ind.set_index(n.buses.index[n.buses.carrier == "AC"])
 
-    ports["p_set"] = ports["fraction"].apply(
-        lambda frac: shipping_hydrogen_share
-        * frac
-        * navigation_demand
+    ports["p_set"] = (
+        shipping_hydrogen_share
+        * ports["fraction"]
+        * ports["country"].map(navigation_demand)
         * efficiency
         * 1e6
         / 8760
@@ -1472,8 +1468,12 @@ def add_shipping(n, costs, energy_totals, ports_fn):
     if shipping_hydrogen_share < 1:
         shipping_oil_share = 1 - shipping_hydrogen_share
 
-        ports["p_set"] = ports["fraction"].apply(
-            lambda frac: shipping_oil_share * frac * navigation_demand * 1e6 / 8760
+        ports["p_set"] = (
+            shipping_oil_share
+            * ports["fraction"]
+            * ports["country"].map(navigation_demand)
+            * 1e6
+            / 8760
         )
 
         n.madd(
