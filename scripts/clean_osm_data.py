1961f50d0a9fda89cb6ffe42a903afc45fb9d6d1
CaterinaCara <caterina.cara@polito.it>
2025-10-09 14:35:23 +0200

    update clean osm data

--- scripts/clean_osm_data.py
+++ scripts/clean_osm_data.py
@@ -892,7 +892,7 @@ def load_network_data(network_asset, data_options):
         loaded_df2 = gpd.read_file(custom_path)
         loaded_df = pd.concat([loaded_df1, loaded_df2], ignore_index=True)
     
-    elif cleaning_data_options == "ignore":
+    elif cleaning_data_options == "none":
         loaded_df1 = gpd.read_file(input_files[network_asset])
         loaded_df = gpd.GeoDataFrame(columns=loaded_df1.columns)
 
