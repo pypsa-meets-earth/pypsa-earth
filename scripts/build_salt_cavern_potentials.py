8c78ac873ba1fd72e844dceb670530b95ff59d44
doneachh <anton.achhammer@oth-regensburg.de>
2026-02-16 07:17:33 -0800

    added SPDX

--- scripts/build_salt_cavern_potentials.py
+++ scripts/build_salt_cavern_potentials.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: PyPSA-Earth and PyPSA-Eur Authors
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 # -*- coding: utf-8 -*-
 """
 Build salt cavern potentials for hydrogen storage.
