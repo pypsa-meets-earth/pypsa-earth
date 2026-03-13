cceb864461ff17e100b02cf611863d1b9ac31a4c
ekatef <e.v.kasilova@gmail.com>
2026-03-13 14:06:06 +0100

    Add a cutout checker

---
+++ test/utils/cutout_checker.py
@@ -0,0 +1,50 @@
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import os
+import xarray as xr
+
+cutout_fl_path
+
+data = xr.open_dataset(
+    os.path.join(cutout_fl_path)
+)
+
+## Basic plotting
+
+Quickly cross-check functionality
+
+data["wnd100m"].mean("time").plot.contourf(cmap=plt.cm.Blues)
+data["temperature"].mean("time").plot.contourf(cmap=plt.cm.autumn)
+
+
+## Advanced plotting
+
+Make sure the spatial distribution makes sense for a region of interest
+
+import cartopy
+
+### Plot wind
+fig, ax = (
+    plt.subplots(figsize=(8, 4), subplot_kw={"projection": ccrs.PlateCarree()})
+)
+data["wnd100m"].mean("time").plot.contourf(cmap=plt.cm.Blues)
+ax.add_feature(cartopy.feature.BORDERS, color="gray", linewidth=0.5)
+ax.coastlines()
+plt.savefig("cutout_wind.jpeg")
+
+### Plot solar
+fig, ax = (
+    plt.subplots(figsize=(8, 4), subplot_kw={"projection": ccrs.PlateCarree()})
+)
+data["influx_direct"].mean("time").plot.contourf(cmap=plt.cm.Oranges)
+ax.add_feature(cartopy.feature.BORDERS, color="gray", linewidth=0.5)
+ax.coastlines()
+
+### Plot temperature
+fig, ax = (
+    plt.subplots(figsize=(8, 4), subplot_kw={"projection": ccrs.PlateCarree()})
+)
+data["temperature"].mean("time").plot.contourf(cmap=plt.cm.summer)
+ax.add_feature(cartopy.feature.BORDERS, color="gray", linewidth=0.5)
+ax.coastlines()
+plt.savefig("cutout_tas.jpeg")
