7051729739e7426c7e2901ab755f4ed31b1a9e0d
Sermisha Narayana <sermisha.narayana@openenergytransition.org>
2026-06-19 14:51:18 +0530

    code: Added some checks and balances

--- scripts/non_workflow/config_update_cli.py
+++ scripts/non_workflow/config_update_cli.py
@@ -2,7 +2,6 @@ import typer
 from rich.console import Console
 from rich.panel import Panel
 from rich.columns import Columns
-from typing_extensions import Annotated
 import yaml
 from enum import Enum
 
@@ -15,7 +14,14 @@ class UserGroup(str, Enum):
     client = "Client"
     custom = "Custom"
 
-def load_config_file(config_path):
+def ask(text: str, default: str="") -> str:
+    styled_text = typer.style(f"➔ {text}", fg=typer.colors.YELLOW, bold=True)
+    if default != "":
+        return typer.prompt(styled_text, default=default)
+    else:
+        return typer.prompt(styled_text)
+
+def load_config_file(config_path: str) -> dict:
     with open(config_path, "r") as file:
         try:
             config = yaml.safe_load(file)
@@ -24,11 +30,10 @@ def load_config_file(config_path):
             print(f"Error parsing YAML file: {error}")
 
 
-def save_config_file(config_path, config_data):
+def save_config_file(config_path: str, config_data: dict[dict]) -> None:
     with open(config_path, "w") as file:
         yaml.safe_dump(config_data, file, default_flow_style=False)
         print(f"Saved the file to {config_path}")
-
 
 
 def flatten_dict(nested_dict: dict, separator: str = ".", current_path: str = "") -> dict:
@@ -71,22 +76,57 @@ def unflatten_dict(flat_dict: dict, separator: str = ".") -> dict:
     return nested_dict
 
 @app.command("config-setup")
-def config_setup(user_groups_config):
-    config_path="config.default.yaml"
-    config=load_config_file(config_path)
+def config_setup():        
+    console.print(style="dim")
+    console.rule("[bold magenta] EDIT CONFIG PARAMETERS [/bold magenta]")
 
-    user_group=typer.prompt("Select user group", default=[x.value for x in UserGroup])
+    # Load user groups
+    user_groups_path="user_groups.yaml"
+    user_groups_config=load_config_file(user_groups_path)
+    # Prompt user to identify his/her user group
+    user_group=ask("Select user group", default=[x.value for x in UserGroup])
+
+    # Prompt user for config file locations
+    config_path=ask("Enter config file name to modify", default="config.default.yaml")
+    console.print(style="dim") 
+    try:
+        config=load_config_file(config_path)
+    except FileNotFoundError:
+        console.print("[bold red] No such file found. \n Exitting the application [/bold red]")
+        raise typer.Exit()
+
+    # Filter config file for the required params based on user group
     config_options=user_groups_config[user_group]
-    reqd_config={x:config[x] for x in config_options} 
+    # Read config file
+    try:
+        reqd_config={x:config[x] for x in config_options} 
+    except KeyError:
+        console.print("[bold red] Mismatch in choice of user group and config file. \n Exitting the application [/bold red]")
+        raise typer.Exit()
+    
+    console.print("[bold]Instructions[/bold]\n")
+    console.print("If a config entry is a nested dictionary, the child param name is represented the parent param name separated by a '.'.\n")
+    console.print(style="dim")
+    console.print("For example, \nenable:\n\tretrieve_databundle:\n")
+    console.print(style="dim")
+    console.print("is represented as [bold]enable.retrieve_databundle[/bold].")
+    console.print(style="dim")
+    console.print(style="dim")
+
+    # Flatten nested dictionaries in the config parameters
     flattened_options=flatten_dict(reqd_config)
 
-    updated_config=reqd_config.copy()
+    # Iterate through the config parameters
+    updated_config=flattened_options.copy()
     for option in flattened_options:
-        updated_value=typer.prompt(f"Enter new value for {option} [Press Enter to skip]",default=flattened_options[option])
-        updated_config[option]=updated_value
+        updated_value=ask(f"Enter new value for {option} [Press Enter to skip]",default=flattened_options[option])
+        console.print(style="dim") 
 
-    config_save_path=typer.prompt(f"Enter name for updated config file [Press Enter to skip]", default="config.cli_updated.yaml")
-    
+        if updated_value != flattened_options[option]:
+            updated_config[option]=updated_value
+
+    # Save updated config file
+    config_save_path=ask(f"Enter name for updated config file [Press Enter to skip]", default="config.cli_updated.yaml")
     save_config_file(config_save_path,unflatten_dict(updated_config))
 
 @app.callback(invoke_without_command=True)
@@ -120,15 +160,14 @@ def main(ctx: typer.Context):
     # Print the menu title and options
     console.rule("[bold magenta] MAIN MENU [/bold magenta]")
     console.print(Columns(panels, padding=(1, 2)))
-    choice=typer.prompt("Select option 1-4 to proceed further[/yellow]")
+    choice=ask("Select option 1-4 to proceed further")
 
     if choice == "2":
-        user_groups_path="user_groups.yaml"
-        user_groups_config=load_config_file(user_groups_path)
-        config_setup(user_groups_config)
+        config_setup()
+    else:
+        console.print("[bold magenta] Feature still under development. Please check again later [/bold magenta]")
+        raise typer.Exit()
 
 
-if __name__ == "__main__":
-
-    
+if __name__ == "__main__":    
     app()
