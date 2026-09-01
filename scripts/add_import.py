20cad15c8bcbb7491beabfc49a2372f31d5160f0
Anton Achhammer <132910766+doneachh@users.noreply.github.com>
2026-09-01 10:39:08 +0200

    Add licensing information to add_import.py

--- scripts/add_import.py
+++ scripts/add_import.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText:  PyPSA-Earth Authors
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# -*- coding: utf-8 -*-
+
 import logging
 
 import geopandas as gpd
